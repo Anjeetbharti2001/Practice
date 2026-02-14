025c97d4a5a71e9c19bccf04eab5077e53053416
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-14 10:33:38 +0530

    Practice array sum

---
+++ Learn.java
@@ -0,0 +1,10 @@
+public class Learn{
+    public static void main(String args[]){
+        int arr[] = {4,7,8,9,2,4};
+        int sum = 0;
+        for(int i = 0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
