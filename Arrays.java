e61f81e9d643cc489267ea3b2003552e10f1f759
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-17 07:36:07 +0530

    Practice sum of element array

---
+++ Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main(String args[]){
+        int arr[] = {3,8,2,3,4,5};
+        int sum = 0;
+        for(int i = 0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+    
+}
