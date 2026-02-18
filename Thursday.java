2f490a62c773db4b6dc4505cea38391dbabb79bf
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-18 09:33:31 +0530

    count odd number

--- Thursday.java
+++ Thursday.java
@@ -1,20 +1,13 @@
-import java.util.*;
+//count  odd number in Array 
 public class Thursday{
     public static void main(String args[]){
-        // Search an element in Array
-        int arr[] = {4,5,2,6,8};
-        int key = 2;
-        boolean found = false;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == key){
-                System.out.println(" Element found at index :-" + i);
-                found = true;
-                break;
+        int arr[] = {4,2,6,8,2,3};
+        int count = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] %  2 !=0){// odd check
+               count++;
             }
         }
-        if (!found ){
-
-            System.out.println("Element is not found:-");
-        }
+        System.out.println("Total odd number :-" + count );
     }
 }
