d1fd1f33d8980dff9bec59f3580056860b8c4feb
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-18 09:07:39 +0530

    sorted Array

--- Arrays1.java
+++
@@ -1,9 +0,0 @@
-// check if array is sorted(accending order)
-import java.util.*;
-public class Arrays1{
-    public static void main(String args[]){
-        int arr[] = {2,6,8,2,3,5,1};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-}
--- Thursday.java
+++ Thursday.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Thursday{
     public static void main(String args[]){
+        // check if array is sorted (acending order)
         int arr[] = {9,8,7,6,5,4,3,2,1};
          Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
