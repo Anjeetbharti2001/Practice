f2cdeacb62571c50b4e58209cdcbef55b862d56b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-18 08:44:21 +0530

    sorted array

---
+++ Thursday.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Thursday{
+    public static void main(String args[]){
+        int arr[] = {9,8,7,6,5,4,3,2,1};
+         Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
