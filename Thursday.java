dc407a62154d42802f0b6b7b9a2a8374577f6057
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-18 10:00:32 +0530

    all array element print

--- Thursday.java
+++ Thursday.java
@@ -1,13 +1,9 @@
-// Find the minimum element in array
+// Print the all Array element 
 public class Thursday{
     public static void main(String args[]){
-        int arr[] = {2,5,6,7,3,7,9};
-        int min = arr[0];
+        int arr[] = {3,7,9,3,6};
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] < min){
-                min = arr[i];
-            }
+            System.out.println(arr[i]);
         }
-        System.out.println("Minimum element i array:-" + min);
     }
 }
