026624d2a82230aa6d161d15bc743725a5b64d59
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-19 10:24:02 +0530

    practice find max element

--- Today.java
+++ Today.java
@@ -1,12 +1,12 @@
 public class Today{
     public static void main(String args[]){
-        // Sum of array element 
-        int arr[] = {4,7,8,3,7,2};
-        int sum = 0; 
+        // find the largest element in array
+        int arr[] = {1, 5,3,6,2};
+             int max = arr[0];
         for(int i = 0; i<arr.length; i++){
-            sum += arr[i];
+            if(arr[i] > max)
+                max = arr[i];
         }
-        System.out.println(sum);
-        
+        System.out.println(max);
     }
 }
