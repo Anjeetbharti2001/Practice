35da4ac9debdc2dad1e6e0b01cdba278f35bd964
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-19 15:25:58 +0530

    exist element in array

--- Today.java
+++ Today.java
@@ -1,13 +1,20 @@
 public class Today{
     public static void main(String args[]){
-        // count even number in array element
-        int arr[] = { 3,6,8,8,3,5};
-        int count = 0;
+        // check if element exist in array
+        int arr[] = {4,7,8,3,9};
+        int key = 8; // jis bh element ko check karna ho 
+        boolean found = false;
         for(int i = 0; i<arr.length; i++){
-            if( arr[i] % 2 == 0){
-                count++;
+            if(arr[i] == key){
+               found = true; 
+                break; 
             }
         }
-        System.out.println(count);
+        if(found){
+            System.out.println("Element exist i array");
+        }else{
+            System.out.println("Element not found");
+        }
+
     }
 }
