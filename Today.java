ea191a4897869e9522955b47c94bcbe17e431a04
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-19 09:56:01 +0530

    practice sum

--- Today.java
+++ Today.java
@@ -1,8 +1,12 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,7,8,3,9,4};
+        // Sum of array element 
+        int arr[] = {4,7,8,3,7,2};
+        int sum = 0; 
         for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+            sum += arr[i];
         }
+        System.out.println(sum);
+        
     }
 }
