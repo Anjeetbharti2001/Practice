21a8c85c9eb082f0d569a3f866539091ab0d662a
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-19 08:57:34 +0530

    print array practice

---
+++ Today.java
@@ -0,0 +1,8 @@
+public class Today{
+    public static void main(String args[]){
+        int arr[] = {4,7,8,3,9,4};
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
