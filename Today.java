f8f8ad449b8c780411104ffec23f65e1d36a8ed3
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-20 08:51:46 +0530

    for the register college data a student

--- Today.java
+++ Today.java
@@ -1,7 +1,15 @@
 public class Today{
     public static void main(String args[]){
-        // Print the multiple variable  sum 
-        int a = 2, b = 4, c =9;
-        System.out.println(a + b + c);
+        // Student college data for registration 
+        String name = " Anjeet bharti";
+        int age = 25;
+        int num = 2101292045;
+        float fee = 25.5f;
+        char grade = 'b';
+        System.out.println("Student name :-" + name);
+        System.out.println("Student register num:-" + num);
+        System.out.println("student age:-" + age);
+        System.out.println("Student college fee:-" + fee);
+        System.out.println("college grade:-" + grade);
     }
 }
