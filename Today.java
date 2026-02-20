e04682b0f96184ae1aa3b1b8117b94ccce220ad0
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-20 08:03:57 +0530

    sum of 2 number

--- Today.java
+++ Today.java
@@ -1,20 +1,8 @@
 public class Today{
     public static void main(String args[]){
-        // check if element exist in array
-        int arr[] = {4,7,8,3,9};
-        int key = 8; // jis bh element ko check karna ho 
-        boolean found = false;
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] == key){
-               found = true; 
-                break; 
-            }
-        }
-        if(found){
-            System.out.println("Element exist i array");
-        }else{
-            System.out.println("Element not found");
-        }
-
+        int a = 3;
+        int b = 10;
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
