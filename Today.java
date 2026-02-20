76d77fca753eb83a6adfc9e26ed2f07c358aa4ec
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-20 08:28:18 +0530

    print multiple variable of sum

--- Today.java
+++ Today.java
@@ -1,7 +1,7 @@
 public class Today{
     public static void main(String args[]){
-        //print variable
-        String name = "Anjeet bharti";
-        System.out.println("hello " + name);
+        // Print the multiple variable  sum 
+        int a = 2, b = 4, c =9;
+        System.out.println(a + b + c);
     }
 }
