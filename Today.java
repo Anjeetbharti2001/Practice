e161918c9eaf4333c6f37bbb906a56ed8655b5ed
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-21 09:48:15 +0530

    max element find

--- Today.java
+++ Today.java
@@ -1,43 +1,13 @@
-// public class Today{
-//     public static void main(String args[]){
-//         int arr[] = {3,6,8,0, 3,7,1};
-        
-//         for(int i = 0; i<arr.length -1 ; i++){
-//             for(int j=0; i<arr.length-1-i; i++){
-//                 if(arr[j] > arr[j + 1]){
-//                     // Swap
-//                     int temp = arr[j];
-//                     arr[j] = arr[j + 1];
-//                     arr[j+1] = temp;
-//                 }
-//             }
-//         }
-//         // Print sorted array
-//         for(int i = 0; i<arr.length; i++){
-//             System.out.print(arr[i] + " ");
-//         }
-//     }
-// }
-
-public class Today {
-    public static void main(String args[]) {
-        int arr[] = {3, 6, 8, 0, 3, 7, 1};
-        
-        // Bubble sort implementation
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+public class Today{
+    public static void main(String args[]){
+        //Find the largest element i array
+        int arr[] = {2,6,9,0,3};
+        int max = arr[0];
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i]  > max){
+                max = arr[i];
             }
         }
-        
-        // Sorted array print
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        System.out.println(max);
     }
 }
