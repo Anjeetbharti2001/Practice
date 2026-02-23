47aaa2899065e97074dd41fceb0edf3e79bc3f91
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-23 08:45:20 +0530

    found min array

--- Today.java
+++ Today.java
@@ -1,13 +1,12 @@
 public class Today{
     public static void main(String args[]){
-        // Find out the largest number in array
-        int arr[] = {3,6,8,9};
-        int max = arr[0];
+        int arr[] = {4,8,9,4};
+        int min = arr[0];
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
+            if(arr[i] < min ){
+                min = arr[i];
             }
         }
-        System.out.println(max);
+        System.out.println(min);
     }
 }
