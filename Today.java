aea49b97fa002433174c5b67f489cb454766c800
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-23 09:00:43 +0530

    count even number

--- Today.java
+++ Today.java
@@ -1,12 +1,12 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,8,9,4};
-        int min = arr[0];
+        int arr[] = {5,7,8,3,4};
+        int count = 0;
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] < min ){
-                min = arr[i];
+            if(arr[i] % 2 == 0){
+                count++;
             }
         }
-        System.out.println(min);
+        System.out.println(count);
     }
 }
