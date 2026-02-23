28aec2ea1b951d2b43e6697a8158770d6143ee9d
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-23 11:45:23 +0530

    css project

--- Thursday.java
+++
@@ -1,9 +0,0 @@
-// Print the all Array element 
-public class Thursday{
-    public static void main(String args[]){
-        int arr[] = {3,7,9,3,6};
-        for(int i = 0; i<arr.length; i++){
-            System.out.println(arr[i]);
-        }
-    }
-}
