819c5630b423a4ccece2fde798fc7c5dac40abc3
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-23 08:25:21 +0530

    sum of array

--- Today.java
+++ Today.java
@@ -1,19 +1,11 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,6,8,0,3,7,1};
+        // Sum of arr Element in array
+        int arr[] = {3,6,8,3,4};
+        int sum = 0;
         for(int i = 0; i<arr.length; i++){
-            for(int j = 0; i<arr.length - 1 - i; i++){
-               if(arr[i] > arr[j + 1]){
-                //swap
-                int temp = arr[j];
-                arr[j] = arr[j];
-                arr[j + 1] = temp;
-               }
-            }
+            sum += arr[i];
         }
-        //print sorted array
-        for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
+        System.out.println(sum);
     }
 }
