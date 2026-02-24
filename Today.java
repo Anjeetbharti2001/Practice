0a93905608d6ea662d7a35baf49fcafa219db648
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-24 08:23:11 +0530

    count odd number

--- Today.java
+++ Today.java
@@ -1,9 +1,9 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {7,3,5,7,4};
-        int count = 0; 
+        int arr[] = {4,7,9,2,4};
+        int count = 0;
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] % 2 == 0){
+            if(arr[i] % 2 != 0){// check odd number
                 count++;
             }
         }
