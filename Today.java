9f0ebb0b2c828563b786a6aec7e08534bb24af91
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-24 17:57:22 +0530

    length of array

--- Today.java
+++ Today.java
@@ -1,19 +1,7 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] ={1,2,3,4,5,6,7,8};
-        int start = 0;
-        int end = arr.length-1;
-        while(start < end){
-            // swap
-            int term = arr[start];
-            arr[start] = arr[end];
-            arr[end] = term;
-            start++;
-            end--;
-        }
-        //print reverse array
-        for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
+        String name = "Anjeet bharti";
+        System.out.println("The length of Array:-" + name.length());
+
     }
 }
