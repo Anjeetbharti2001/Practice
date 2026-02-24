4cb3aee6b543b84156366cb24bfdcc6a166732ba
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-24 18:14:05 +0530

    String test

--- Today.java
+++ Today.java
@@ -1,7 +1,7 @@
 public class Today{
     public static void main(String args[]){
         String name = "Anjeet bharti";
-        System.out.println("The length of Array:-" + name.length());
-
+    //    System.out.println(name.toLowerCase());
+    System.out.println(name.toUpperCase());
     }
 }
