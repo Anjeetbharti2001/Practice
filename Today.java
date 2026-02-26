01ea3f145bbad5367931a7c9c02942bbaa51f936
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-26 09:17:15 +0530

    practice code

--- Today.java
+++ Today.java
@@ -1,17 +1,36 @@
+// public class Today{
+//     public static void main(String args[]){
+//         int arr[] = {9,8,7,6,5,4,3,2,1};
+//         for(int i = 0; i<arr.length; i++){
+//             for(int j = 0; j<arr.length - 1 - i; j++){
+//                 if(arr[j] > arr[j + 1]){
+//                     // swap
+//                     int temp = arr[i];
+//                     arr[j] = arr[j + 1];
+//                     arr[j + 1] = temp;
+//                 }
+//             }
+//         }
+//         //print sorted array
+//         for(int i = 0; i<arr.length; i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
 public class Today{
     public static void main(String args[]){
-        int arr[] = {9,8,7,6,5,4,3,2,1};
+        int arr[] = {0,9,8,7,6,5,4,3,2,1};
         for(int i = 0; i<arr.length; i++){
-            for(int j = 0; j<arr.length - 1 - i; j++){
+            for(int j = 0; j<arr.length - 1 - i; i++){
                 if(arr[j] > arr[j + 1]){
-                    // swap
+                    //swap
                     int temp = arr[i];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    arr[j] =arr[j + 1];
+                    arr[j + 1 ] = temp;
                 }
             }
         }
-        //print sorted array
+        // Print sorted array
         for(int i = 0; i<arr.length; i++){
             System.out.print(arr[i] + " ");
         }
