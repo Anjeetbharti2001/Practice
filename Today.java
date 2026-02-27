4e9d2a4975c5ed9e3da0bf73ed8501658775a996
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-27 07:15:25 +0530

    max element

--- Today.java
+++ Today.java
@@ -1,10 +1,13 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,9,5,7,4,5,6};
-        int sum = 0;
+        // Find the largest element in array
+        int arr[] = {7,8,9,5,7,3};
+        int max = arr[0];
         for(int i = 0; i<arr.length; i++){
-            sum += arr[i];
+            if(arr[i] > max){
+                max = arr[i];
+            }
         }
-        System.out.println(sum);
+        System.out.println(max);
     }
 }
