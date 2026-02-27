5ded175b2b6106968cfc6475e86fed5f43e4a88e
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-27 07:41:35 +0530

    lenght

--- Today.java
+++ Today.java
@@ -1,12 +1,7 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,6,8,90,8,2};
-        int count = 0; 
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] % 2 != 0){
-                count++;
-            }
-        }
-        System.out.println(count);
+         String consonent = "abcdefghijklmnopqrstuvwxuz";
+         System.out.println(consonent.length());
+
     }
 }
