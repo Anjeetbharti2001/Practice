c94918e10a56dec64efda2b893a970578c5967d9
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-02-27 07:51:59 +0530

    isEmpty

--- Today.java
+++ Today.java
@@ -1,7 +1,8 @@
 public class Today{
     public static void main(String args[]){
-         String consonent = "abcdefghijklmnopqrstuvwxuz";
-         System.out.println(consonent.length());
-
+        String name = "anjeet";
+        String name2 = "";
+        System.out.println(name.isEmpty());
+        System.out.println(name2.isEmpty());
     }
 }
