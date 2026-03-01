e8d43cf27e43cee5606731d79b45da1d959c6553
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-01 07:52:35 +0530

    array

--- Today.java
+++ Today.java
@@ -1,9 +1,7 @@
-// 1,2,3,4,5
-import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,9,2,6,8,4};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int arr[] = {4,8,9,4,5,7,8};
+        int size = arr.length; // length find out kiya
+        System.out.println("Array of the length:- " + size);
     }
 }
