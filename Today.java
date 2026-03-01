6fd9d51873ec0bcaff85deba9e72d10680c6c3d0
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-01 07:27:04 +0530

    even count

--- Today.java
+++ Today.java
@@ -1,14 +1,13 @@
-// 1,2,
+// 1,2,3
 public class Today{
     public static void main(String args[]){
-        // find out the minimum number in array
-        int arr[] = {3,6,9,24,2};
-        int min = arr[0];
+        int arr[] = {4,7,9,0,4,5};
+        int count = 0;
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] < min ){
-                min = arr[i];
+            if(arr[i] % 2 == 0){
+                count++;
             }
         }
-        System.out.println(min);
+        System.out.println("Total count even value:- " + count);
     }
 }
