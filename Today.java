8a67ca7aa17086cab4c86c4d13cea570bdd91975
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-01 07:07:54 +0530

    all practice

--- Today.java
+++ Today.java
@@ -1,6 +1,12 @@
 public class Today{
     public static void main(String args[]){
-        String name = "anjeetbharti";
-        System.out.println(name.toUpperCase());
+        // Sum of all element i array
+        int arr[] = {6,9,0,3,5,6};
+        int sum = 0; 
+        for(int i = 0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+
     }
 }
