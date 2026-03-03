9e1ee3998a1c8a70ef0e37337c86a731c1e7117f
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-03 08:28:11 +0530

    size of ARRAY

--- Today.java
+++ Today.java
@@ -1,20 +1,7 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {1,2,3,4,5,6};
-        int start = 0; 
-        
-        int end = arr.length - 1;
-        while(start < end){
-            //Swap
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-        // Print reversed array
-        for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
+        int arr[] = {3,7,8,9,4,3};
+        int size = arr.length;
+        System.out.println("Total length of array:- " + size);
     }
 }
