30729d54e34c2d6bd7dd75e7066274b1ad8c6799
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-03 07:51:59 +0530

    reversed

--- Today.java
+++ Today.java
@@ -1,8 +1,20 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,7,9,0,4,3};
-       for(int i = 0; i<arr.length; i++){
-        System.out.print(arr[i] + " ");
-       }
+        int arr[] = {1,2,3,4,5,6};
+        int start = 0; 
+        
+        int end = arr.length - 1;
+        while(start < end){
+            //Swap
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        // Print reversed array
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
