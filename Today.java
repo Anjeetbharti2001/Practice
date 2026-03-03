ec7f510456472425292e9258739528de2c28ecbc
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-03 09:13:42 +0530

    empty

--- Today.java
+++ Today.java
@@ -1,33 +1,13 @@
-// public class Today{
-//     public static void main(String args[]){
-//         int arr[] = {4,6,7,9,0,2,3};
-
-
-//         int sum = 0;
-
-//         // loop to add all element
-//         for(int i = 0; i<arr.length; i++){
-//              sum = sum + arr[i];
-//         }
-//         // calculate average element 
-//         double average = (double) sum / arr.length;
-//         System.out.println("Total average element:-" + average);
-        
-//     }
-// }
-
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,6,7,8,9};
+        // String str = " ";
+        String str = "Anjeet";
 
-        int sum = 0;
-        
-        //loop to add all element
-        for(int i = 0; i<arr.length; i++){
-            sum = sum + arr[i];
+        if(str .isEmpty()){
+            System.out.println("String is not empty");
+        }else{
+            System.out.println("String is empty");
         }
-        // calculate average element 
-        double average = (double) sum / arr.length;
-        System.out.println("Total avearge element :- " + average);
+        System.out.println(str);
     }
 }
