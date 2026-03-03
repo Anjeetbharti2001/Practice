fa5ec5baa19e717ba6ae7022f342271b49286ddb
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-03 08:59:46 +0530

    average total

--- Today.java
+++ Today.java
@@ -1,12 +1,33 @@
+// public class Today{
+//     public static void main(String args[]){
+//         int arr[] = {4,6,7,9,0,2,3};
+
+
+//         int sum = 0;
+
+//         // loop to add all element
+//         for(int i = 0; i<arr.length; i++){
+//              sum = sum + arr[i];
+//         }
+//         // calculate average element 
+//         double average = (double) sum / arr.length;
+//         System.out.println("Total average element:-" + average);
+        
+//     }
+// }
+
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,3,5,8,9};
-        int count = 0;
+        int arr[] = {3,6,7,8,9};
+
+        int sum = 0;
+        
+        //loop to add all element
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] % 2 == 0 ){
-                count++;
-            }
+            sum = sum + arr[i];
         }
-        System.out.println(count);
+        // calculate average element 
+        double average = (double) sum / arr.length;
+        System.out.println("Total avearge element :- " + average);
     }
 }
