98ed5da94ba4b41d6f3d340d1b7080bc5a859857
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-03 07:32:06 +0530

    print array

--- Today.java
+++ Today.java
@@ -1,9 +1,8 @@
-import java.util.Arrays;
-
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,9,2,3,4,1,2};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int arr[] = {4,7,9,0,4,3};
+       for(int i = 0; i<arr.length; i++){
+        System.out.print(arr[i] + " ");
+       }
     }
 }
