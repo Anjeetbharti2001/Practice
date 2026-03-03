9cf2b1adfcbe41ada674cfbeeae46fb18d086fd0
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-03 20:01:30 +0530

    length

--- Today.java
+++ Today.java
@@ -1,12 +1,8 @@
 public class Today{
     public static void main(String args[]){
         String str = "Anjeet";
-        String str2 = "Bharti";
 
-        if(str.equals(str2)){
-            System.out.println("String is equal ");
-        }else{
-            System.out.println("String is not equal");
-        }
+        int count = str.length();
+        System.out.println("Total character" + count);
     }
 }
