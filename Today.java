5bed5cb0fdffd81308b5653be0dd862c83837a52
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-04 15:59:36 +0530

    even

--- Today.java
+++ Today.java
@@ -1,10 +1,12 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {2,4,5,6,7};
-        int sum = 0;
+        int arr[] = {2,4,5,6,7,7};
+        int count = 0;
         for(int i = 0; i<arr.length; i++){
-            sum += arr[i];
+            if(arr[i] % 2 == 0){
+                count++;
+            }
         }
-        System.out.println(sum);
+        System.out.println(count);
     }
 }
