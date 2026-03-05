4dff6b6c176863fc52073e24f8deadd746524614
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-05 09:24:32 +0530

    max value

--- Today.java
+++ Today.java
@@ -1,12 +1,12 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,4,5,6,7,8,2};
-        int count = 0; 
-        for(int i = 0 ; i<arr.length; i++){
-            if(arr[i] % 2 != 0){
-                count++;
+        int arr[] = {2,5,6,7,8,9};
+        int max =arr[0];
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
             }
         }
-        System.out.println(count);
+        System.out.println("Largest value in array:-" + max);
     }
 }
