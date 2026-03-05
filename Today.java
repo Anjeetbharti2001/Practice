77dfcd668bfde867a152f46ab0983d023a65317c
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-05 09:33:18 +0530

    min value

--- Today.java
+++ Today.java
@@ -1,12 +1,12 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {2,5,6,7,8,9};
-        int max =arr[0];
+        int arr[] = {3,5,6,7,8,3,2};
+        int min = arr[0];
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
+            if(arr[i] < min ){
+               min = arr[i];
             }
         }
-        System.out.println("Largest value in array:-" + max);
+        System.out.println("Minimum value in this array:-" + min);
     }
 }
