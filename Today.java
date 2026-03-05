e335c5669eabb6a0bd2377c50d3c8ce29c5a8058
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-05 09:12:21 +0530

    even

--- Today.java
+++ Today.java
@@ -1,9 +1,9 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,5,6,7,8,8};
-        int count = 0;
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] % 2 == 0){
+        int arr[] = {3,4,5,6,7,8,2};
+        int count = 0; 
+        for(int i = 0 ; i<arr.length; i++){
+            if(arr[i] % 2 != 0){
                 count++;
             }
         }
