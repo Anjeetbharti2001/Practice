962658c1d982a90c215dbfa70194aa6074aec700
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-06 07:10:26 +0530

    sort

--- Today.java
+++ Today.java
@@ -1,19 +1,8 @@
+import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int arr[] = {2,4,6,7,8,9};
-        int start = 0; 
-        int end = arr.length - 1;
-        while (start < end){
-            // swap
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-        // Print reverse array
-        for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
+        int arr[] = {3,5,6,7,8,9};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
