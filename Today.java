f75dfc6c2b0dfb3cf4c211f734ebd85b485a9391
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-06 08:04:55 +0530

    lst and fst char find

--- Today.java
+++ Today.java
@@ -1,11 +1,11 @@
 public class Today{
     public static void main(String args[]){
         String str = "Anjeet";
+        
+        char first = str.charAt(0);
+        char last = str.charAt(str.length() - 1);
 
-        char arr[] = str.toCharArray();
-
-        for(int i = 0; i<arr.length; i++){
-            System.out.println(arr[i]);
-        }
+        System.out.println("character first letter:-" + first);
+        System.out.println("character last :- " + last);
     }
 }
