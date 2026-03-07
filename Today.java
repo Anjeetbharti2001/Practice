82efc0d684e2f753d88f0e9e404c4535315c5d66
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-07 09:12:55 +0530

    adda

--- Today.java
+++ Today.java
@@ -1,29 +1,8 @@
-import java.util.*;
+public class Today{
+    public static void main(String args[]){
+        String str = "I love Anjeet ";
 
-public class Today {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-
-        int sum = 0;
-        int avg = 0;
-
-        int[] marks = new int[3];   // array create
-
-        // Store data in array
-        for(int i = 0; i < 3; i++){
-            System.out.print("Enter mark: ");
-            marks[i] = sc.nextInt();
-        }
-
-        // Read data and calculate sum
-        for(int i = 0; i < 3; i++){
-            System.out.println("enter marks:");
-            marks[i] = sc.nextInt();
-            sum = sum + marks[i];
-        }
-
-        avg = sum / 3;
-
-        System.out.println("Average marks: " + avg);
+        str = str.replace("0 ","self  ");
+        System.out.println(str);
     }
 }
