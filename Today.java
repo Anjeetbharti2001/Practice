448e5651a818b0bd21d3835d47dbf34d23eb3c7e
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-08 09:33:11 +0530

    sort

--- Today.java
+++ Today.java
@@ -1,10 +1,9 @@
+import java.util.Arrays;
+
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,5,6,7,8,9,0,3};
-        int sum = 0;
-        for(int i = 0; i<arr.length; i++){
-            sum += arr[i];
-        }
-        System.out.println(sum);
+        int arr[] = {4,6,7,8,4,7,83,2,4};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
