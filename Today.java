0e0dabc856fb24d41df0f5dfb4f6673d9e0098a2
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-08 09:10:26 +0530

    even number

--- Today.java
+++ Today.java
@@ -1,6 +1,13 @@
 public class Today{
     public static void main(String args[]){
-        String str = "anjeet bharti";
-        System.out.println(str.toUpperCase());
+        int arr[] = {4,6,7,8,9,23,2};
+        int count = 0 ;
+        
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] % 2 == 0){
+              count++;
+            }
+        }
+        System.out.println(count);
     }
 }
