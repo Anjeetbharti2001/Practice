0ac873274dfba6364e4d7adbb67d89baff716c55
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-09 10:15:00 +0530

    compare

--- Today.java
+++ Today.java
@@ -1,6 +1,12 @@
+// check is the string of equal yes or not 
 public class Today{
     public static void main(String args[]){
-        String str = "ANJEET";
-        System.out.println(str.toLowerCase());
+        String str1 = "bharti";
+        String str2 = "Bharti";
+        if(str1.equals(str2)){
+            System.out.println("String is equal");
+        }else{
+            System.out.println("String is not equal");
+        }
     }
 }
