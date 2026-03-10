a5887c4ab2dd670b061c72a34892b7720a29e3d9
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-10 09:03:27 +0530

    sum

--- Today.java
+++ Today.java
@@ -1,6 +1,10 @@
 public class Today{
     public static void main(String args[]){
-        String str = "ANJEET BHARTI FROM BIHAR";
-        System.out.println(str.toLowerCase());
+        int arr[] = {2,7,9,4,6,7};
+        int sum = 0; 
+        for(int i = 0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
     }
 }
