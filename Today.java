4a9e24b620236e25d351e23942b31aac108e11a6
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-11 08:10:27 +0530

    true or false

--- Today.java
+++ Today.java
@@ -1,12 +1,9 @@
 public class Today{
+
+    public static boolean ispowerofTwo(int n){
+        return n > 0 && (n & (n-1)) == 0;
+    }
     public static void main(String args[]){
-        int arr[] = {3,6,8,9,3,2};
-        int count = 0;
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] % 2 != 0){
-                count++;
-            }
-        }
-        System.out.println(count);
+      System.out.println(ispowerofTwo(16));
     }
 }
