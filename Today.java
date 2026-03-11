2716b6a2f14b0a7ba4c4f437b66717ba8278dacc
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-11 10:18:27 +0530

    distribute string

--- Today.java
+++ Today.java
@@ -1,14 +1,9 @@
 public class Today{
     public static void main(String args[]){
-        String str = "Java Is Easy To Learn";
-        String [] words =str.split(" ");
+        String str = "1a2b3c4";
 
-        for(int i = 0; i<words.length; i++){
-            String first = words[i].substring(0,1).toLowerCase();
-            String rest = words[i].substring(1);
-            words[i] = first + rest;
+        for(int i = 0; i< str.length(); i++){
+            System.out.println(str.charAt(i));
         }
-        String result = String.join(" ", words);
-        System.out.println(result);
     }
 }
