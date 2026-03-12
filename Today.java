c3a350bd2d799da6c6b644b9df8e3d32f7d32074
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-12 07:40:47 +0530

    max

--- Today.java
+++ Today.java
@@ -1,9 +1,14 @@
+// find the largest element in array
 public class Today{
     public static void main(String args[]){
-        String str = "1a2n3j4e5e6t";
+        int arr[] = {2,4,5,8,93};
+        int max = arr[0];
 
-        for(int i = 0; i<str.length(); i++){
-            System.out.println(str.charAt(i));
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > max ){
+                max = arr[i];
+            }
         }
+        System.out.println("Array in maximum number:-" + max);
     }
 }
