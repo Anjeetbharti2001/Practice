6da389134ccf13cb0bdcc054b0a6e965695071b3
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-14 07:46:54 +0530

    min

--- Today.java
+++ Today.java
@@ -1,14 +1,14 @@
 public class Today{
     public static void main(String args[]){
-        // Find the largest number in array
-        int arr[] = {2,5,7,8,9};
-        int max = arr[0];
+        // Find the minimum number in array
+        int arr[] = {2,5,6,7,8,9};
+        int min = arr[0];
 
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] > max ){
-                max = arr[i];
+            if(arr[i] < min){
+                min = arr[i];
             }
         }
-        System.out.println(max);
+        System.out.println(min);
     }
 }
