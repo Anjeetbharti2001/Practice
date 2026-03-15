e214106b691fa5013e5c56873e5c367dd8643e2a
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-15 10:01:47 +0530

    count

--- Today.java
+++ Today.java
@@ -1,8 +1,12 @@
-import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int arr[] = {9,5,7,2,1,5,8};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int arr[] = {2,6,9,2,4,5};
+        int count = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] %2 == 0){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
