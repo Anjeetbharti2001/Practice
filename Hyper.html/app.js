5e7888e9683d014549bdadb7bdf8c399bff38d94
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-15 09:27:08 +0530

    sum

--- Hyper.html/app.js
+++
@@ -1,7 +0,0 @@
-let id = setInterval(() => {
-    console.log("hello anjeet");
-}, 2000);
-
-setTimeout(() => {
-    clearInterval(id);
-}, 1000);
--- Today.java
+++ Today.java
@@ -1,8 +1,29 @@
-import java.util.*;
+// // Reverse array 
+// public class Today{
+//     public static void main(String args[]){
+//         int arr[] = {2,4,6,7,8,9,4,34,6};
+//         int start = 0;
+//         int end = arr.length -1;
+//         while(start < end){
+//             // Swap
+//             int temp = arr[start];
+//             arr[start] = arr[end];
+//             arr[end] = temp;
+//             start++;
+//             start--;
+//         }
+//         //print reversed array
+//         for(int i = 0; i<arr.length; i++){
+//             System.out.println(arr[i] + " ");
+//         }
+//     }
+// }
+
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,8,93,6,1,2,8};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int a = 12;
+        int b = 3;
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
