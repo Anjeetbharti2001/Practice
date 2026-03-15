1dfaf6ab8f1368bbb65f462a50efc295330b30ad
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-15 09:41:50 +0530

    sum array

--- Today.java
+++ Today.java
@@ -1,23 +1,10 @@
-// Reverse array
 public class Today{
     public static void main(String args[]){
-        int arr[] = {2,4,6,7,8,9,4,34,6};
-        int start = 0;
-        int end = arr.length - 1;
-
-        while(start < end){
-            // Swap
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-
-            start++;
-            end--;   // yaha mistake tha
+        int arr[] = {6,7,8,9,9};
+        int sum = 0;
+        for(int i = 0; i<arr.length; i++){
+            sum += arr[i];
         }
-
-        // print reversed array
-        for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
+        System.out.println(sum);
     }
 }
