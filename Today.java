83f32831f0a9549c8b298be0d06f17a9ca7c0b69
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-16 10:22:53 +0530

    max

--- Today.java
+++ Today.java
@@ -1,12 +1,13 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {2,7,9,5,34,6,7};
-        int count = 0;
+        int arr[] = {3,6,8,9,4,6};
+        int max = arr[0];
+
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] %2 == 0){
-                count++;
+            if(arr[i] > max){
+                max = arr[i];
             }
         }
-        System.out.println(count);
+        System.out.println(max);
     }
 }
