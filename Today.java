7bc5f5f4f2ca55ff30324d710c157ab1643215a4
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-16 10:14:37 +0530

    count even value

--- Today.java
+++ Today.java
@@ -1,8 +1,12 @@
-import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int arr[] = {5,8,0,2,3,5,4};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int arr[] = {2,7,9,5,34,6,7};
+        int count = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] %2 == 0){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
