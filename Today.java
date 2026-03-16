c52222a129caea8e7f8d2ecff92c837cffff598e
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-16 10:06:57 +0530

    average value

--- Today.java
+++ Today.java
@@ -1,19 +1,13 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,10,1,2,6};
-        int start = 0;
-        int end = arr.length -1; 
-
-        while(start < end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-        // print reversed array
+        int arr[] = {3,6,9,3,6};
+        int sum = 0; 
+        // Loop to add all element 
         for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+            sum = sum + arr[i];
         }
+        // Calculate average
+        double average = (double) sum/arr.length;
+        System.out.println("Avarage:-" + average);
     }
 }
