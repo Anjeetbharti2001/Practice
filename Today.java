f49752006a044b6d1001f2f96c341a91dbf0f1c7
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-17 07:57:19 +0530

    revesed

--- Today.java
+++ Today.java
@@ -1,13 +1,20 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,6,8,9,4,6};
-        int max = arr[0];
+        int arr[] = {6,8,10,2,5,3,1};
+        int start = 0;
+        int end = arr.length-1;
 
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
-            }
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
-        System.out.println(max);
+        //print reversed array
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
+// output reversed arrar:- 1,3,5,2,10,8,6
