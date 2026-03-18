b4b10ffbb84d59c1fc14cbe5f7820ed3f90d9123
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-18 09:13:53 +0530

    count even

--- Today.java
+++ Today.java
@@ -1,24 +1,12 @@
-import java.util.*;
 public class Today{
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-
-
-        System.out.println("Enter size:-");
-        int n = sc.nextInt();
-
-        
-        int arr[] = new int[n];
-
-        int sum = 0; 
-        System.out.println("enter element:-");
-        for(int i = 0; i< n ; i++){
-            System.out.println(".()");
-            arr[i] = sc.nextInt();
-            sum = sum + arr[i];
+        int arr[] = {3,9,15,2,6,9};
+        int count = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] %2 == 0){
+                count++;
+            }
         }
-        double avg = (double) sum/n;
-        System.out.println("average element:-" + avg);
-
+        System.out.println(count);
     }
 }
