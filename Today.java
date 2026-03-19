fa22af56d700d8370bdd4f95a1639d3a59fbb230
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-19 08:10:41 +0530

    string check qual yes or no

--- Today.java
+++ Today.java
@@ -1,6 +1,11 @@
 public class Today{
     public static void main(String args[]){
-        String str = "ANJEET";
-        System.out.println("This is a LowerCase String:-" + str.toLowerCase());
+        String str1 = "anjeet";
+        String str2 = "bharti";
+        if(str1.equals(str2)){
+            System.out.println("String is equal");
+        }else{
+            System.out.println("String is not equal");
+        }
     }
 }
