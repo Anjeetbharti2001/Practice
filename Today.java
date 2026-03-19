e7e9f6f05d02d771ddca47f5c8182c709921bdfd
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-19 07:52:04 +0530

    string empty

--- Today.java
+++ Today.java
@@ -1,7 +1,10 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {2,4,5,6,7,8};
-        int size = arr.length;
-        System.out.println("Array of length:-" + size);
+        String str = "";
+        if(str.isEmpty()){
+            System.out.println("String is empty");
+        }else{
+            System.out.println("String is not empty");
+        }
     }
 }
