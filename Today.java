c8bc1f04cec72bb99e3f8f7b9ca85b85879f5ecc
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-19 08:18:49 +0530

    sort

--- Today.java
+++ Today.java
@@ -1,11 +1,8 @@
+import java.util.*;
 public class Today{
     public static void main(String args[]){
-        String str1 = "anjeet";
-        String str2 = "bharti";
-        if(str1.equals(str2)){
-            System.out.println("String is equal");
-        }else{
-            System.out.println("String is not equal");
-        }
+        int arr[] = {3,6,83,14,63,2,1,57,3};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
