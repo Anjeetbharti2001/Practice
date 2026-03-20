22b81018c44cceebe0360bd8bbccfbd7ea3d983a
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-20 07:30:59 +0530

    string compare

--- Today.java
+++ Today.java
@@ -1,6 +1,12 @@
+// compare two string 
 public class Today{
     public static void main(String args[]){
-        String str = "Anjeet bharti";
-        System.out.println(str.length());
+        String str = "Anjeet";
+        String str1 = "Bharti";
+        if(str.equals(str1)){
+            System.out.println("String is equals");
+        }else{
+            System.out.println("String is not equals");
+        }
     }
 }
