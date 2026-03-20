e8dd95f8a79b076ceaf9854d3b459be5070f8531
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-20 06:49:38 +0530

    rvd array

--- Today.java
+++ Today.java
@@ -1,8 +1,19 @@
 public class Today{
     public static void main(String args[]){
-        String str = "anjeet";
-
-        int count = str.length();
-        System.out.println("Total character:-" + count);
+        int arr[] = {4,7,9,3,3,5,6};
+        int start = 0;
+        int end = arr.length -1;
+        //swaps
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        // print reversed array
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
