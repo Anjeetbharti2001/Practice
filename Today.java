0d0662aa310b0563ac5a952d2c6c2562cfc65744
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-20 07:08:00 +0530

    sort

--- Today.java
+++ Today.java
@@ -1,12 +1,8 @@
+import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,6,9,0,2};
-        int sum = 0;
-
-        for(int i = 0; i<arr.length; i++){
-            sum = sum + arr[i];
-        }
-        double avg = (double)sum / arr.length;
-        System.out.println("Average of sum :-" + avg);
+        int arr[] = {5,7,8,9,3,5,1,2};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
