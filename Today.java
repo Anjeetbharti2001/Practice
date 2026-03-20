c40c0cd5ec8af979dcd48abcba8e7e869f5dfa79
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-20 07:03:18 +0530

    avg

--- Today.java
+++ Today.java
@@ -1,19 +1,12 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,7,9,3,3,5,6};
-        int start = 0;
-        int end = arr.length -1;
-        //swaps
-        while(start < end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-        // print reversed array
+        int arr[] = {3,6,9,0,2};
+        int sum = 0;
+
         for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+            sum = sum + arr[i];
         }
+        double avg = (double)sum / arr.length;
+        System.out.println("Average of sum :-" + avg);
     }
 }
