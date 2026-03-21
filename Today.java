e5baec779fafb98f1362284e6a49cc6f6a69a78c
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-21 08:39:13 +0530

    length str

--- Today.java
+++ Today.java
@@ -1,11 +1,6 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {2,6,8,9};
-        int sum = 0; 
-        for(int i = 0; i<arr.length; i++){
-            sum = sum + arr[i];
-        }
-        double avg = (double) sum/arr.length;
-        System.out.println("Total average sum :-" + avg);
+        String str = "Anjeetbhartibihar";
+        System.out.println(str.length());
     }
 }
