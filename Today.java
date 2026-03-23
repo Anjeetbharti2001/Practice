cf03f2b8554d94540957646a6a19a647a2221cfc
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-23 08:31:11 +0530

    division

--- Today.java
+++ Today.java
@@ -6,9 +6,11 @@ public class Today{
            int a = 8;
            int b = 2;
         // Multiplication 
-         int multi = a *b;
-           System.out.println(multi);
+        //  int multi = a *b;
+        //    System.out.println(multi);
         // Division 
+        int div = a / 2;
+        System.out.println(div);
         // Modulus 
         //Increment 
         // Decrement
