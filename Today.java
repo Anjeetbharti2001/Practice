3121a76b949aeae4ae14eecd2bad259736baf113
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-23 08:46:55 +0530

    decrement

--- Today.java
+++ Today.java
@@ -1,7 +1,7 @@
 public class Today{
     public static void main(String args[]){
-        int a = 5;
-        System.out.println(a++);
+        int a = 78;
+        System.out.println(a--);
         System.out.println(a);
     }
 }
