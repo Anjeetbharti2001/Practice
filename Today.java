32c4ed6d74cc98ac5e696367cbfe1023d54e4b6c
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-24 08:34:26 +0530

    avg element

--- Today.java
+++ Today.java
@@ -1,18 +1,14 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {0,9,8,7,6,5,4,3,2,1};
-        int start = 0;
-        int end = 0; 
-        while(start < end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-        //reversed print
+        // average number
+        int arr[] = {3,6,9,2,4};
+        int sum = 0;
+
         for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+            sum = sum + arr[i];
         }
+        // average
+        double avg = (double) sum / arr.length;
+        System.out.println(" Total average number:-" + avg);
     }
 }
