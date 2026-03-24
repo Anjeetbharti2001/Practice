bb89b58f7bc069c6398d3cb9d2417e7430059aa5
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-24 08:17:05 +0530

    sort array

--- Today.java
+++ Today.java
@@ -1,6 +1,11 @@
+import java.util.*;
+
 public class Today{
+    
     public static void main(String args[]){
-        String str = "anjeet bharti";
-        System.out.println(str.toUpperCase());
+       // Sort arrays
+       int arr[] = {3,9,4,7,82,5,7};
+       Arrays.sort(arr);
+       System.out.println(Arrays.toString(arr));
     }
 }
