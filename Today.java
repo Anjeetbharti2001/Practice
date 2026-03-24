7ecb712abc6ed6ea910c4f7ca30e2f5a8b2d404e
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-24 08:24:40 +0530

    revesed arrays

--- Today.java
+++ Today.java
@@ -1,11 +1,18 @@
-import java.util.*;
-
 public class Today{
-    
     public static void main(String args[]){
-       // Sort arrays
-       int arr[] = {3,9,4,7,82,5,7};
-       Arrays.sort(arr);
-       System.out.println(Arrays.toString(arr));
+        int arr[] = {0,9,8,7,6,5,4,3,2,1};
+        int start = 0;
+        int end = 0; 
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        //reversed print
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
