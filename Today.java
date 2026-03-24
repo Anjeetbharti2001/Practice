0fe8df80d9b4e956bfbd745914f363753b676ff4
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-24 07:57:00 +0530

    increment

--- Today.java
+++ Today.java
@@ -1,12 +1,7 @@
 public class Today{
-    static void printNum(int n){
-        if(n == 0){
-            return ;
-        }
-        printNum(n-1);
-        System.out.print(n + " ");
-    }
     public static void main(String args[]){
-          printNum(5);
+        int a = 5;
+        System.out.println(a++);
+        System.out.println(a);
     }
 }
