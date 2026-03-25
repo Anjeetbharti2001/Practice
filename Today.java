5690098f8ded1a1cad1dd71225d875da072a2207
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-25 08:24:35 +0530

    average number

--- Today.java
+++ Today.java
@@ -1,19 +1,11 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,6,8,9};
-        int start = 0; 
-        int end = arr.length-1;
-        
-        while(start < end){
-            int temp = arr[start];
-            arr[end] = arr[end];
-            arr[start] = temp;
-            start++;
-            end--;
-        }
-        // print revesed array
+        int arr[] = {3,6,7,8,9};
+        int sum = 0;
         for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+                sum = sum + arr[i];
         }
+        double avg = (double) sum/arr.length;
+        System.out.println(avg);
     }
 }
