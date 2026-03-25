df1ae7ed822dd1d81491ded83f9648e118c23579
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-25 08:20:15 +0530

    reversed arrays

--- Today.java
+++ Today.java
@@ -1,13 +1,19 @@
 public class Today{
-    // Print factorial of an number 
-    static int factorial(int n){
-        if(n == 0 || n == 1){
-            return 1;
-        }
-        return n *factorial(n -1);
-    }
     public static void main(String args[]){
-        System.out.println(factorial(5));
+        int arr[] = {4,6,8,9};
+        int start = 0; 
+        int end = arr.length-1;
         
+        while(start < end){
+            int temp = arr[start];
+            arr[end] = arr[end];
+            arr[start] = temp;
+            start++;
+            end--;
+        }
+        // print revesed array
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
