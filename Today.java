55ae87da30cf84d8e20add3d3d4e067b27186fc5
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-25 08:04:37 +0530

    factorial number

--- Today.java
+++ Today.java
@@ -1,13 +1,13 @@
 public class Today{
-    // sum first number 
-    static int sum (int n){
-        if(n == 1){
+    // Print factorial of an number 
+    static int factorial(int n){
+        if(n == 0 || n == 1){
             return 1;
         }
-        return n + sum(n - 1);
+        return n *factorial(n -1);
     }
     public static void main(String args[]){
-        System.out.println(sum(5));
+        System.out.println(factorial(5));
         
     }
 }
