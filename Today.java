e1d61c6d7328cf845f2a90c833dd5de611703e64
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-26 09:22:01 +0530

    sum addition

--- Today.java
+++ Today.java
@@ -1,13 +1,9 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,7,9,9,3};
-        int sum = 0;
+        int a = 6;
+        int b = 8;
 
-        for(int i = 0; i<arr.length; i++){
-            sum = sum + arr[i];
-        }
-        // average element in array
-        double avg = (double) sum/arr.length;
-        System.out.println(avg);
+        int sum = a + b;
+        System.out.print(sum);
     }
 }
