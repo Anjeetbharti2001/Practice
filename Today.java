263d972625987110f7506869510b20e8bbea897c
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-26 09:03:20 +0530

    reverse array print

--- Today.java
+++ Today.java
@@ -1,11 +1,20 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,6,7,8,9};
-        int sum = 0; 
+        int arr[] = {4,7,8,8,9,0};
+        int start = 0;
+        int end = arr.length -1;
 
-        for(int i = 0; i<arr.length; i++){
-            sum += sum + arr[i];
+        while(start <end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
         }
-        System.out.println(sum);
+        //print reversed
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
