f1108cbfb155ba4f6e74468b3fb939f0722d5b13
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-27 07:26:07 +0530

    odd number in array

--- Today.java
+++ Today.java
@@ -1,10 +1,10 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,9,05,4,3,2,6};
+        int arr[] = {3,6,8,9,0,2};
         int count = 0;
 
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] %2 == 0){
+            if(arr[i] %2 !=0){
                 count++;
             }
         }
