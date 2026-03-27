2789fa6e3d4ca229c6cc3486858b79ef954b8b1d
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-27 07:47:04 +0530

    arrays reversed

--- Today.java
+++ Today.java
@@ -1,13 +1,20 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,5,6,7,8,6};
-        int min = arr[0];
+        int arr[] = {4,6,7,8,9,0};
+        int start = 0;
+        int end = arr.length -1;
 
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] < min ){
-                min = arr[i];
-            }
+        while(start < end ){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
         }
-        System.out.println(min);
+        // print reversed arrays
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
