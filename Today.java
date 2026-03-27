c8fac7445200216f5ce616f1cb216da4af39e647
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-27 07:51:45 +0530

    index find out

--- Today.java
+++ Today.java
@@ -1,20 +1,6 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,6,7,8,9,0};
-        int start = 0;
-        int end = arr.length -1;
-
-        while(start < end ){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-
-            start++;
-            end--;
-        }
-        // print reversed arrays
-        for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
+        int arr[] = {3,5,6,7,8};
+        System.out.print(arr[0] + " ");
     }
 }
