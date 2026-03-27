ad46a3bd10f73e873cc75e7906ab7866f29247d4
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-27 07:17:03 +0530

    sum array

--- Today.java
+++ Today.java
@@ -1,9 +1,11 @@
 public class Today{
     public static void main(String args[]){
-        int a = 6;
-        int b = 8;
+        int arr[] = {4,7,8,9};
+        int sum = 0;
 
-        int sum = a + b;
-        System.out.print(sum);
+        for(int i = 0; i<arr.length; i++){
+            sum = arr[i] + sum;
+        }
+        System.out.println(sum);
     }
 }
