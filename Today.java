fd47b9f81a78d3fe6c853e028ed5df7a138eafa2
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-28 09:30:13 +0530

    sum variable

--- Today.java
+++ Today.java
@@ -1,21 +1,8 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,6,7,8,9,2};
-        int start = 0; 
-        int end = arr.length -1;
-
-       while(start < end){
-        int temp = arr[start];
-          arr[start] = arr[end];
-          arr[end] = temp;
-
-          start++;
-          end--;
-       }
-       //Reversed arrays in java code 
-       for(int i = 0; i<arr.length; i++){
-        System.out.print(arr[i] + " ");
-       }
-      
+        int a = 9;
+        int b = 3;
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
