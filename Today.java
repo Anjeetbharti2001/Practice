dfb2f12733e540f581d71d848f743f94d7208d4d
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-29 09:18:33 +0530

    add yani sum

--- Today.java
+++ Today.java
@@ -1,11 +1,7 @@
 public class Today{
-    static int sum (int n){
-        if(n == 1){
-            return 1;
-        }
-        return n + sum(n -1);
-    }
     public static void main(String args[]){
-        System.out.println(sum(5));
-    }
+        int a = 2;
+        int b = 2;
+        int sum = a + b;
+    System.out.println(sum);    }
 }
