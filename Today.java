4d10820708cbf1751ba6111cfb1ab6c0073cbd85
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-29 09:07:43 +0530

    print n to 1

--- Today.java
+++ Today.java
@@ -1,14 +1,12 @@
-//power of number x^n
 public class Today{
-    static int power(int x, int n){
-        if(n == 0){
-            return 1; // base case
+     static void printRev(int n){
+            if(n == 0){
+                return ;
+            }
+            System.out.print(n);
+            printRev(n-1);
         }
-        return x * power(x, n-1);
-    }
     public static void main(String args[]){
-        int x = 2;
-        int n = 3;
-        System.out.println(power(x,n));
+       printRev(8);
     }
 }
