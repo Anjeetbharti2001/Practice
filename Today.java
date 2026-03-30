5be26bb3178a6b5b516ee219e9de459c847f9394
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-30 08:35:54 +0530

    factorial

--- Today.java
+++ Today.java
@@ -1,13 +1,25 @@
+// public class Today{
+//     // print number 1 to n 
+//     static void printNum(int n){
+//         if(n == 0){
+//             return;
+//         }
+//         printNum(n - 1);
+//         System.out.println(n);
+//     }
+//     public static void main(String args[]){
+//           printNum(9);
+//     }
+// }
+
 public class Today{
-    // print number 1 to n 
-    static void printNum(int n){
-        if(n == 0){
-            return;
+    static int factorial(int n){
+        if(n == 0 || n == 1){
+            return 1;
         }
-        printNum(n - 1);
-        System.out.println(n);
+        return n * factorial (n -1);
     }
     public static void main(String args[]){
-          printNum(9);
+        System.out.println(factorial(5));
     }
 }
