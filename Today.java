e54c76a76ddaa3741efa40ccf1a46c31ea93d111
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-31 08:49:15 +0530

    count of the character in string

--- Today.java
+++ Today.java
@@ -1,12 +1,7 @@
 public class Today{
     public static void main(String args[]){
-        String str1 = "ANJEET";
-        String str2 = "ANJEET";
-
-        if(str1.equals(str2)){
-            System.out.println("Plindrome is equal ");
-        }else{
-            System.out.println("Palindrome is not equals");
-        }
+        String str = "Anjeet";
+        int count = str.length();
+        System.out.println("Total character :-" + count);
     }
 }
