36d60f2911272ccc9ec632d9d08f74f85380420b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-31 08:40:24 +0530

    compare to string equal

--- Today.java
+++ Today.java
@@ -1,6 +1,12 @@
 public class Today{
     public static void main(String args[]){
-        String str = "anjeet";
-        System.out.println(str.toUpperCase());
+        String str1 = "ANJEET";
+        String str2 = "ANJEET";
+
+        if(str1.equals(str2)){
+            System.out.println("Plindrome is equal ");
+        }else{
+            System.out.println("Palindrome is not equals");
+        }
     }
 }
