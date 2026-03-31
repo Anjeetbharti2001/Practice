80908ae244477bf7a6e7c5626606a964b9b3fcf7
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-03-31 08:29:14 +0530

    toLowerCase

--- Today.java
+++ Today.java
@@ -1,14 +1,6 @@
 public class Today{
-    static int fib(int n){
-        if(n == 0 ) return 0;
-        if(n == 1) return 1;
-        
-        return fib(n-1) + fib(n-2);
-    }
     public static void main(String args[]){
-        int n = 5;
-        for(int i = 0; i<n; i++){
-            System.out.print(fib(i) + " ");
-        }
+        String str = "ANJEET";
+        System.out.print(str.toLowerCase());
     }
 }
