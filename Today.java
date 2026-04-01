e90ec610d5d547ad1e4216def4767484db053cf6
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-01 08:32:34 +0530

    fibannacci

--- Today.java
+++ Today.java
@@ -1,7 +1,15 @@
 public class Today{
+    static int fib(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        return fib(n - 1) + fib(n - 2);
+    }
     public static void main(String args[]){
-        String str = "I Love Anjeet";
-        str = str.replace(" ", "");
-        System.out.println(str);
+        int n = 5; 
+
+        for(int i = 0 ; i< n ; i++){
+            System.out.print(fib(i) + " ");
+        }
     }
 }
