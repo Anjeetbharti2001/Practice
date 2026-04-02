ef5803809e2924093b201cbde54b3c8cd458eab8
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-02 08:05:45 +0530

    print stack element

--- Today.java
+++ Today.java
@@ -1,10 +1,12 @@
 import java.util.*;
 public class Today{
     public static void main(String args[]){
-        Stack <Integer> s = new Stack<>();
+        Stack<Integer> s = new Stack<>();
 
-        if(s.isEmpty()){
-            System.out.println("Stack is empty");
-        }
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        System.out.println(s);
     }
 }
