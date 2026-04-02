2abcfd7b0a7d6b223991522f0f4f3afcad654f88
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-02 08:00:48 +0530

    check stack empty yes or not

--- Today.java
+++ Today.java
@@ -1,15 +1,10 @@
 import java.util.*;
 public class Today{
     public static void main(String args[]){
-        Stack<Integer> s = new Stack<>();
+        Stack <Integer> s = new Stack<>();
 
-        s.push(10);
-        s.push(20);
-        s.push(30);
-
-        System.out.println("Top :" + s.peek()); //30
-
-        s.pop();
-        System.out.println("after pop :" + s.peek()); // 20
+        if(s.isEmpty()){
+            System.out.println("Stack is empty");
+        }
     }
 }
