0db1414e60c015c82251f34a126d9d91c257dc25
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-02 08:42:46 +0530

    even number

--- Today.java
+++ Today.java
@@ -1,8 +1,13 @@
-import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,8,9,2,4,5,1};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int arr[] = {3,5,6};
+        int count = 0; 
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] % 2 == 0){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
