d7fe441575234229736ca2b64494443249f5a295
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-02 09:02:41 +0530

    average element in array

--- Today.java
+++ Today.java
@@ -1,20 +1,12 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,5,6,7,8,4,2};
-        int start = 0;
-        int end = arr.length-1;
+        int arr[] = {2,4,6,7};
+        int sum = 0;
 
-        while(start < end ){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-
-            start++;
-            end--;
-        }
-        //reversed array
         for(int i = 0; i<arr.length; i++){
-            System.out.println(arr[i] + " ");
+            sum = sum + arr[i];
         }
+        double avg = (double) sum/arr.length;
+        System.out.println("total avg " + avg);
     }
 }
