a87dc34e4c03d0c655c7220ee746cdeb4e1a1bd3
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-02 08:30:23 +0530

    lowerCase

--- Today.java
+++ Today.java
@@ -1,12 +1,6 @@
 public class Today{
     public static void main(String args[]){
-        String str = "Anjeet";
-        String str2 = "Anjeetbharti";
-
-        if(str.equals(str2)){
-            System.out.println("String is equals");
-        }else{
-            System.out.println("String is not equal");
-        }
+        String str = " ANJEET BHARTI";
+        System.out.println(str.toLowerCase());
     }
 }
