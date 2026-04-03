eba636eb62f36f9c4316c525aa39735a4705b15b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-03 08:31:44 +0530

    arrays length

--- Today.java
+++ Today.java
@@ -1,12 +1,8 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {2,4,6,7};
-        int sum = 0;
-
-        for(int i = 0; i<arr.length; i++){
-            sum = sum + arr[i];
-        }
-        double avg = (double) sum/arr.length;
-        System.out.println("total avg " + avg);
+        int arr[] = {4,5,6,7,8};
+        
+        int length = arr.length;
+        System.out.println("Total of arrays length:-" + length);
     }
 }
