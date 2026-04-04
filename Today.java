21c64abd79c1fa7a1b7ed3e9c31050cda56ae133
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-04 08:16:07 +0530

    string length

--- Today.java
+++ Today.java
@@ -1,8 +1,6 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,5,6,7,7};
-        int length = arr.length;
-
-        System.out.println(length);
+        String str = "Anjeet bharti";
+        System.out.println(str.length());
     }
 }
