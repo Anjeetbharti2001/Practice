6f1b719865fc797a2bc38a536fe2c477aca4eb87
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-04 09:06:26 +0530

    minimum element in arrays

--- Today.java
+++ Today.java
@@ -1,13 +1,13 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,7,8,4,1,3,2};
-        int max = arr[0];
+        int arr[] = {4,6,8,9,2};
+        int min = arr[0];
 
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] > max ){
-                max = arr[i];
+            if(arr[i] < min){
+                min = arr[i];
             }
         }
-        System.out.println(max);
+        System.out.println(min);
     }
 }
