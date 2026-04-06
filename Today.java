6a97de953c3573d03dc07308007eca5b9fcba596
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-06 10:05:26 +0530

    find out of module number in java

--- Today.java
+++ Today.java
@@ -1,22 +1,10 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {1,2,3,4,5,5,6,6,7,7,8,8};
-        int start = 0;
-        int end = arr.length-1;
 
-        while(start < end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            
+        int a = 9;
+        int b = 2;
 
-            start++;
-            end--;
-        }
-        // reversed in arrays
-        for(int i = 0; i<arr.length; i++){
-            System.out.println(arr[i] + " ");
-        }
-
+        int sum = a % b;
+         System.out.println(sum);
     }
 }
