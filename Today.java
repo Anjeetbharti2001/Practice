0647baf3fb94ef593ce9f748932352148f06e4e8
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-06 09:37:03 +0530

    sum of java natural number

--- Today.java
+++ Today.java
@@ -1,11 +1,12 @@
 public class Today{
-    static int fact(int n){
-        if(n == 0 || n == 1){
-            return 1;
+    // Sum of n natural number in java
+    static int sum(int n){
+        if(n == 0){
+            return 0;
         }
-        return n*fact(n - 1);
+        return n + sum(n - 1);
     }
     public static void main(String args[]){
-       System.out.println(fact(7));
+       System.out.println(sum(7));
     }
 }
