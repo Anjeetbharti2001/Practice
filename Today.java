a5cfa5cce7cdeb05129e2852802a851181021311
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-06 10:01:38 +0530

    reverse arrays in java

--- Today.java
+++ Today.java
@@ -1,10 +1,22 @@
 public class Today{
-    static int power(int x, int n){
-        if(n == 0) return 1;
-
-        return x * power(x, n-1);
-    }
     public static void main(String args[]){
-         System.out.println(power(6,3)); 
+        int arr[] = {1,2,3,4,5,5,6,6,7,7,8,8};
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            
+
+            start++;
+            end--;
+        }
+        // reversed in arrays
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i] + " ");
+        }
+
     }
 }
