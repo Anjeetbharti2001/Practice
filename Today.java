b1ba8e4b93d8a4b5bf41e5c772d2c8945e3a47c5
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-06 09:54:20 +0530

    power off number in  java

--- Today.java
+++ Today.java
@@ -1,11 +1,10 @@
 public class Today{
-    static int  fib(int n){
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        
-        return fib(n -1) + fib(n - 2);
+    static int power(int x, int n){
+        if(n == 0) return 1;
+
+        return x * power(x, n-1);
     }
     public static void main(String args[]){
-        System.out.println(fib(6));
+         System.out.println(power(6,3)); 
     }
 }
