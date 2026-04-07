b52800f3cb9a884e18e64e5427e0de7c802a0637
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-07 10:08:11 +0530

    reversed arrays in arrays

--- Today.java
+++ Today.java
@@ -1,13 +1,22 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] ={3,5,7,8,9,9};
-        int count = 0; 
+        int arr[] = {1,3,4,5,6,6,7,8,7};
+        int start = 0;
+        int end = arr.length -1;
 
-        for(int i = 0 ; i<arr.length; i++){
-            if(arr[i] % 2 !=0){
-                count++;
-            }
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            
+
+            start++;
+            end--;
+
         }
-        System.out.println("odd number count in arrays element:- " + count);
+        // reversed arrays 
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
