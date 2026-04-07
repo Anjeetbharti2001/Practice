3738168b43ed1572292d0b38eb93dc61c6f02096
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-07 10:11:58 +0530

    Sorted arrays

--- Today.java
+++ Today.java
@@ -1,22 +1,8 @@
+import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int arr[] = {1,3,4,5,6,6,7,8,7};
-        int start = 0;
-        int end = arr.length -1;
-
-        while(start < end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            
-
-            start++;
-            end--;
-
-        }
-        // reversed arrays 
-        for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
+        int arr[] = {3,8,4,9,1,3,7,3};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
