70d7fb43b181b9982bec6d0bca09286031d4ab16
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-08 09:42:34 +0530

    factorial number in recursion method

--- Today.java
+++ Today.java
@@ -1,12 +1,11 @@
 public class Today{
-    static void printNum(int n){
-        if(n == 0){
-            return ;
+    static int factorial(int n){
+        if(n == 0 || n == 1){
+            return 1;
         }
-        printNum(n -1);
-        System.out.println(n);
+        return n * factorial(n -1);
     }
     public static void main(String args[]){
-        printNum(5);
+        System.out.println(factorial(5));
     }
 }
