28a0678854b472e98c03e4f7302d2843e651af4f
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-09 13:57:48 +0530

    LowerCase String

--- Today.java
+++ Today.java
@@ -1,8 +1,6 @@
-import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int  arr[] = {4,6,7,8,9};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        String str = "Anjeet";
+        System.out.println(str.toLowerCase());
     }
 }
