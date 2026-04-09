b6b5a64b9fb274b77ec7582a63a3ae6dc6e0c9e4
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-09 15:00:08 +0530

    odd number

--- Today.java
+++ Today.java
@@ -1,13 +1,13 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,5,6,7,7};
-        int count = 0; 
+          int arr[] = {3,5,6,7,8};
+         int  count = 0;
 
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] %2 == 0){
+          for(int i = 0; i<arr.length; i++){
+             if(arr[i] % 2 !=0){
                 count++;
-            }
-        }
-        System.out.println(count);
+             }
+          }
+          System.out.println(count);
     }
 }
