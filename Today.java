cf670077d548c661daab11bb5be11647032aea16
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-10 10:36:25 +0530

    Maximum Element in Stack

--- Today.java
+++ Today.java
@@ -1,42 +1,17 @@
-// import java.util.*;
-// public class Today{
-//     public static void main(String args[]){
-//         Stack<Character> st = new Stack<>();
-
-//         for(char ch : s.toCharArray()){
-//             if(ch == '('){
-//                 st.push(ch == ')'){
-//                     if(st.isEmpty()) return false;
-//                     st.pop();
-//                 }
-//             }
-//             return st.isEmpty();
-//         }
-//         public static void main(String[] args){
-//             System.out.println(isValid("()()"));//true
-//         }
-//     }
-// }
 import java.util.*;
+public class Today{
+    public static void main(String args[]){
+        Stack<Integer> st = new Stack<>();
+        st.push(5);
+        st.push(10);
+        st.push(3);
 
-public class Today {
+        int max = Integer.MIN_VALUE;
 
-    public static boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-
-        for(char ch : s.toCharArray()) {
-            if(ch == '(') {
-                st.push(ch);
-            } else if(ch == ')') {
-                if(st.isEmpty()) return false;
-                st.pop();
-            }
+        while(!st.isEmpty()){
+            int val = st.pop();
+            if(val > max ) max = val;
         }
-        return st.isEmpty();
-    }
-
-    public static void main(String[] args) {
-        System.out.println(isValid("()()")); // true
+        System.out.println("Max : " + max);
     }
 }
-
