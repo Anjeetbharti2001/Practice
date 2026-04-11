32a5090baf59fd286959a0b7143e1e61a23b6d20
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-11 10:10:18 +0530

    copy one stack To Another

--- Today.java
+++ Today.java
@@ -1,10 +1,14 @@
-import java.util.Stack;
+import java.util.*;
 public class Today{
     public static void main(String args[]){
-        Stack<Integer> st = new Stack<>();
+        Stack<Integer> st1 = new Stack<>();
+        st1.push(1);
+        st1.push(2);
 
-        if(st.isEmpty()){
-            System.out.println("Stack is Empty");
+        Stack<Integer> st2 = new Stack<>();
+        for(int x : st1){
+            st2.push(x);
         }
+        System.out.println(st2);
     }
 }
