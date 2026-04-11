448034bda1cee49d6f3b192182361e28f85ac1dc
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-11 10:04:07 +0530

    stack is empty

--- Today.java
+++ Today.java
@@ -1,13 +1,10 @@
-import java.util.*;
+import java.util.Stack;
 public class Today{
     public static void main(String args[]){
         Stack<Integer> st = new Stack<>();
-        st.push(1);
-        st.push(2);
-        st.push(3);
 
-        for(int i : st){
-            System.out.println(i);
+        if(st.isEmpty()){
+            System.out.println("Stack is Empty");
         }
     }
 }
