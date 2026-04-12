5219ce562b973a8363d2e113fbf5493eb1dc77e3
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-12 09:13:24 +0530

    stack max number in java

--- Today.java
+++ Today.java
@@ -1,14 +1,17 @@
 import java.util.*;
 public class Today{
     public static void main(String args[]){
-        Stack<Integer> st1 = new Stack<>();
-        st1.push(1);
-        st1.push(2);
+        Stack<Integer> st = new Stack<>();
+        st.push(5);
+        st.push(10);
+        st.push(3);
 
-        Stack<Integer> st2 = new Stack<>();
-        for(int x : st1){
-            st2.push(x);
+        int max = Integer. MIN_VALUE;
+
+        while(!st.isEmpty()){
+            int val = st.pop();
+            if(val > max) max = val;
         }
-        System.out.println(st2);
+        System.out.print("max : " + max);
     }
 }
