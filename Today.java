a61ed8a85b93d88ddb0c9485f2c2985937cd8a27
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-12 09:31:12 +0530

    reversed arrays in java

--- Today.java
+++ Today.java
@@ -1,13 +1,20 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,5,5,8,9};
-        int max = arr[0];
+        int arr[] = {1,3,4,5,6};
+        int start = 0;
+        int end = arr.length -1;
 
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
-            }
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
         }
-        System.out.println(max);
+        // reversed arrays
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i] + " ");
+        }
     }
 }
