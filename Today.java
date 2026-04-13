3acc9b63422516247d5c151fd2d390c6d5fab3a4
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-13 10:05:26 +0530

    Sorted arrays element in java

--- Today.java
+++ Today.java
@@ -1,9 +1,9 @@
 import java.util.*;
+
 public class Today{
     public static void main(String args[]){
-        Stack<Integer> st = new Stack<>();
-        st.push(6);
-        st.push(8);
-        System.out.println("size: - " + st.size());
+        int arr[] = {4,6,7,8,6,7,8,9,34,1,5,7};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
