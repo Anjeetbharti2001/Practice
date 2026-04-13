f3eed24ab6cc240b8643e1a7a09c14ac2778ef2f
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-13 10:11:22 +0530

    reversed arrays in java

--- Today.java
+++ Today.java
@@ -1,9 +1,20 @@
-import java.util.*;
-
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,6,7,8,6,7,8,9,34,1,5,7};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int arr[] = {3,4,6,7,9,2,4,7};
+        int start = 0;
+        int end = arr.length -1;
+
+        while(start < end ){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+        // reversed arrays in java 
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
