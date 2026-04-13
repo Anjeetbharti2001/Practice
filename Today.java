f00536a612bab93718d010a08573aee1bd89b0bb
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-13 10:01:03 +0530

    size element in stack

--- Today.java
+++ Today.java
@@ -2,15 +2,8 @@ import java.util.*;
 public class Today{
     public static void main(String args[]){
         Stack<Integer> st = new Stack<>();
-        st.push(2);
         st.push(6);
-        st.push(7);
-
-        int sum = 0;
-
-        for(int x : st){
-            sum += x;
-        }
-        System.out.println("sum of element in stack:- " + sum);
+        st.push(8);
+        System.out.println("size: - " + st.size());
     }
 }
