3ff9156e8162c37cb2d22c8a9a466f3e0d076f89
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-13 09:31:12 +0530

    peek data without removing

--- Today.java
+++ Today.java
@@ -1,20 +1,10 @@
+import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int arr[] = {1,3,4,5,6};
-        int start = 0;
-        int end = arr.length -1;
-
-        while(start < end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-
-            start++;
-            end--;
-        }
-        // reversed arrays
-        for(int i = 0; i<arr.length; i++){
-            System.out.println(arr[i] + " ");
-        }
+        Stack<Integer> st = new Stack<>();
+        st.push(100);
+        st.push(200);
+        System.out.println(st.peek());
+        System.out.println(st);
     }
 }
