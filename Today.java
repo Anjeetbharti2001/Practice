e3b7e841c73164f7b51375e0a4401f8f1bcd4d93
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-13 09:43:26 +0530

    the empty

--- Today.java
+++ Today.java
@@ -2,9 +2,12 @@ import java.util.*;
 public class Today{
     public static void main(String args[]){
         Stack<Integer> st = new Stack<>();
-        st.push(100);
-        st.push(200);
-        System.out.println(st.peek());
-        System.out.println(st);
+        st.push(1);
+        st.push(2);
+
+        while(!st.isEmpty()){
+            st.pop();
+        }
+        System.out.println("The empty: " + !st.isEmpty());
     }
 }
