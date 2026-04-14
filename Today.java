e4cac7937ba2ac58bf52fbbae1a5d73fa32e490b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-14 09:48:05 +0530

    average element in java

--- Today.java
+++ Today.java
@@ -1,11 +1,14 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {2,6,8,9,3};
-        int sum = 0;
+        // average element in java
+        int arr[] = {3,5,6,7,8,8,9,9};
+        int sum = 0; 
 
         for(int i = 0; i<arr.length; i++){
-            sum += arr[i];
+            sum = sum + arr[i];
         }
-        System.out.println(sum);
+        //average element in java
+        double avg = (double) sum/arr.length;
+        System.out.println(" Average element in arrays:-" + avg);
     }
 }
