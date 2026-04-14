14ef690efd226282b4a6b45a482f606fd7cdaeeb
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-14 09:31:08 +0530

    Arrays is sorted

--- Today.java
+++ Today.java
@@ -1,7 +1,10 @@
-// Find out the length of arrays element ;
-public class Today{
+// Print of arrays element 
+
+import java.util.*;
+public class Today {
     public static void main(String args[]){
-        int arr[] = {3,5,76,7,8,9,9,0};
-        System.out.println(arr[7]);
+        int arr[] = {3,5,7,2,8,1,9,4};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
