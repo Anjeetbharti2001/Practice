7fc501e5c2547c3ac98fec0d49edc2d376544511
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-14 09:39:15 +0530

    sum arrays element

--- Today.java
+++ Today.java
@@ -1,10 +1,11 @@
-// Print of arrays element 
-
-import java.util.*;
-public class Today {
+public class Today{
     public static void main(String args[]){
-        int arr[] = {3,5,7,2,8,1,9,4};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int arr[] = {2,6,8,9,3};
+        int sum = 0;
+
+        for(int i = 0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
     }
 }
