0a02b7d016a6111fe18207e2b46a1fae1484faef
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-14 09:18:04 +0530

    Reversed arrays element in java

--- Today.java
+++ Today.java
@@ -1,20 +1,21 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,4,6,7,9,2,4,7};
+        int arr[] = {2,6,8,9,3,2};
         int start = 0;
         int end = arr.length -1;
 
-        while(start < end ){
+        while(start < end){
             int temp = arr[start];
             arr[start] = arr[end];
             arr[end] = temp;
 
             start++;
             end--;
+
         }
-        // reversed arrays in java 
-        for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+        //Reversed arrays element in java
+        for(int i = 0 ; i<arr.length; i++){
+            System.out.println(arr[i] + " ");
         }
     }
 }
