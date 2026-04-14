4508f7d0ee194ec9decd527aa007e6b3bdc86cc0
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-14 09:55:15 +0530

    uppercase in java

--- Today.java
+++ Today.java
@@ -1,6 +1,6 @@
 public class Today{
     public static void main(String args[]){
         String str = "Anjeet";
-        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
     }
 }
