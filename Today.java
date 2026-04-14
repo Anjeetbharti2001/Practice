88b62a8cba824d9077096b0c2ab13b6bf495a971
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-14 09:53:38 +0530

    lowercase in arrays

--- Today.java
+++ Today.java
@@ -1,14 +1,6 @@
 public class Today{
     public static void main(String args[]){
-        // average element in java
-        int arr[] = {3,5,6,7,8,8,9,9};
-        int sum = 0; 
-
-        for(int i = 0; i<arr.length; i++){
-            sum = sum + arr[i];
-        }
-        //average element in java
-        double avg = (double) sum/arr.length;
-        System.out.println(" Average element in arrays:-" + avg);
+        String str = "Anjeet";
+        System.out.println(str.toLowerCase());
     }
 }
