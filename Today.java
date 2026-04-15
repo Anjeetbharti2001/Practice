6e50c76afeab3cd3be48c0d6e1c219c5e8dc5c21
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-15 10:04:56 +0530

    Sorted arrays

--- Today.java
+++ Today.java
@@ -1,29 +1,8 @@
-// public class Today{
-//     public static void main(String args[]){
-//         int [] p;
-//         p = func();
-//         for(int i = 0; i<=p.length -1; i++){
-//             System.out.println(p[i] + " ");
-//         }
-//         static int[]func()
-//     }
-//     int []arr= {10,20,30,40,50};
-//     return arr;
-// }
-
-
-public class Today {
-    public static void main(String args[]) {
-        int[] p;
-        p = func();
-
-        for(int i = 0; i < p.length; i++) {
-            System.out.print(p[i] + " ");
-        }
-    }
-
-    static int[] func() {
-        int[] arr = {10, 20, 30, 40, 50};
-        return arr;
+import java.util.*;
+public class Today{
+    public static void main(String args[]){
+        int arr[] = {4,66,7,8,9};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
