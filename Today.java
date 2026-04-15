5ca9997cde9d7085c35c15385b64f8fc68485e83
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-15 10:22:00 +0530

    toUpperCase

--- Today.java
+++ Today.java
@@ -1,6 +1,6 @@
 public class Today{
     public static void main(String args[]){
-        String str = "ANJEET BHARTI";
-        System.out.println(str.toLowerCase());
+        String str = "anjeetb";
+        System.out.println(str.toUpperCase());
     }
 }
