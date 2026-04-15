0d5adea06cdad45d8fc7bbf1b34b9c18bc28523c
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-15 10:13:25 +0530

    Average element in arrays

--- Today.java
+++ Today.java
@@ -1,8 +1,13 @@
-import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,66,7,8,9};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int arr[] = {2,6,7,8,9};
+        int sum = 0;
+
+        for(int i = 0; i<arr.length; i++){
+            sum = sum + arr[i];
+        }
+        // average element in arrays
+        double avg = (double)sum/arr.length;
+        System.out.println("Total average element in arrays:-" + avg);
     }
 }
