4d6f1127006e582fae14f72efddb1b45d1f0c77f
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-15 10:18:10 +0530

    TolowerCase

--- Today.java
+++ Today.java
@@ -1,13 +1,6 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {2,6,7,8,9};
-        int sum = 0;
-
-        for(int i = 0; i<arr.length; i++){
-            sum = sum + arr[i];
-        }
-        // average element in arrays
-        double avg = (double)sum/arr.length;
-        System.out.println("Total average element in arrays:-" + avg);
+        String str = "ANJEET BHARTI";
+        System.out.println(str.toLowerCase());
     }
 }
