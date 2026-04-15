d7201ae4d19287121fa029795c47b135ee2aea11
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-15 09:32:45 +0530

    modify number in arrays

--- Today.java
+++ Today.java
@@ -1,6 +1,32 @@
-public class Today{
-    public static void main(String args[]){
-        String str = "Anjeet";
-        System.out.println(str.toUpperCase());
+// public class Today{
+//     public static void main(String args[]){
+//         int arr[] = {55,65,75,56,78,78,90};
+//         int i;
+//         modify(marks);
+
+//         for(i = 0; i<= marks.length -1; i++);
+//         System.out.println(marks[i] + " ");
+//     }
+//     static void modify(int[]m)
+//     int i ;
+//     for(i = 0; i<= m.length- 1; i++)
+//         m[i] = m[i]*2;
+// }
+
+public class Today {
+    public static void main(String args[]) {
+        int arr[] = {55,65,75,56,78,78,90};
+
+        modify(arr);
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void modify(int[] m) {
+        for(int i = 0; i < m.length; i++) {
+            m[i] = m[i] * 2;
+        }
     }
 }
