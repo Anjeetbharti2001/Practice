78d65ecea80efcd4c8cb8f28be6a99b3b3582c9f
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-16 09:22:49 +0530

    sum of element in Stack

--- Today.java
+++ Today.java
@@ -2,9 +2,13 @@ import java.util.*;
 public class Today{
     public static void main(String args[]){
         Stack<Integer> st = new Stack<>();
-        st.push(100);
-        st.push(200);
-        System.out.println(st.peek());
-        System.out.println(st);
+        st.push(1);
+        st.push(2);
+
+        int sum = 0;
+        for(int x : st){
+            sum += x;
+        }
+        System.out.println("Sum :-" + sum);
     }
 }
