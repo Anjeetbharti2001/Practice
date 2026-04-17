f2320c79f8a898ef7e48fd5e0090ee38fbe1a011
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-17 17:20:16 +0530

    variable but name in staring word

--- Today.java
+++ Today.java
@@ -1,7 +1,6 @@
 public class Today{
     public static void main(String args[]){
-        System.out.println(3);
-        System.out.println(358);
-        System.out.println(50000);
+        String name = "Anjeet";
+        System.out.println(name);
     }
 }
