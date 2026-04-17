2a57cdec079946cd32011576ed64693f5d190015
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-17 17:03:15 +0530

    Start java language in java

--- Today.java
+++ Today.java
@@ -1,10 +1,5 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {2,4,6,7};
-        int sum = 0;
-        for(int i = 0; i<arr.length; i++){
-            sum += arr[i];
-        }
-        System.out.print(sum);
+        System.out.println("HI anjeet how are you dear bro ");
     }
 }
