750b53535676f5ca98422691641984fe71c30051
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-17 17:52:21 +0530

    find out in arrays largest element

--- Today.java
+++ Today.java
@@ -1,13 +1,13 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {34,6,7,9,0,2};
-        int count = 0;
+        int arr[] = {3,4,66,77,8,9};
+        int max = arr[0];
 
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] %2 !=0){
-                count++;
+            if(arr[i] > max){
+                max = arr[i];
             }
         }
-        System.out.println(count);
+        System.out.println(max);
     }
 }
