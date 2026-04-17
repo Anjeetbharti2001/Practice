5e1b3403d7301195ca3fba0fa2d53cecf6ecec2a
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-17 17:48:31 +0530

    total count in arrays element odd

--- Today.java
+++ Today.java
@@ -1,10 +1,10 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,6,7,8,9};
+        int arr[] = {34,6,7,9,0,2};
         int count = 0;
-        
+
         for(int i = 0; i<arr.length; i++){
-            if(arr[i] %2 == 0){
+            if(arr[i] %2 !=0){
                 count++;
             }
         }
