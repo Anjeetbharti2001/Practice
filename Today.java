055bf86b27fd45eea104d06d54f5d94274810c95
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-18 17:39:53 +0530

    Reversed a queue

--- Today.java
+++ Today.java
@@ -2,10 +2,22 @@ import java.util.*;
 public class Today{
   public static void main(String args[]){
     Queue<Integer> q = new LinkedList<>();
+
+    Stack<Integer> stack = new Stack<>();
+
     q.add(1);
     q.add(2);
     q.add(3);
 
-    System.out.println("Size of queue: " + q.size());
+    // Moves queue to stack 
+    while(!q.isEmpty()){
+      stack.push(q.remove());
+    }
+
+    // Move back to queue
+    while(!stack.isEmpty()){
+      q.add(stack.pop());
+    }
+    System.out.println("Reversed queue : " + q);
   }
 }
