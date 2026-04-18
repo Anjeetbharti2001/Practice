9b5ba128cfd4f17209e44a32f6ecf1cf4463aae1
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-18 17:30:47 +0530

    Size of Queue

--- Today.java
+++ Today.java
@@ -1,12 +1,11 @@
 import java.util.*;
 public class Today{
   public static void main(String args[]){
-    Queue <Integer> q = new LinkedList<>();
-
+    Queue<Integer> q = new LinkedList<>();
     q.add(1);
     q.add(2);
     q.add(3);
 
-    System.out.println("Size of queue : " + q.size());
+    System.out.println("Size of queue: " + q.size());
   }
 }
