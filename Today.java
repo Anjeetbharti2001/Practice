492669368b1f389569e62195fef3a83a262fe0be
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-19 07:36:41 +0530

    total sum of variable

--- Today.java
+++ Today.java
@@ -1,23 +1,11 @@
-import java.util.*;
 public class Today{
   public static void main(String args[]){
-    Queue<Integer> q = new LinkedList<>();
+    int x = 3;
+    int y = 5;
+    int z = 6;
+    
+    int sum = x + y + z;
+    System.out.println(sum);
 
-    Stack<Integer> stack = new Stack<>();
-
-    q.add(1);
-    q.add(2);
-    q.add(3);
-
-    // Moves queue to stack 
-    while(!q.isEmpty()){
-      stack.push(q.remove());
-    }
-
-    // Move back to queue
-    while(!stack.isEmpty()){
-      q.add(stack.pop());
-    }
-    System.out.println("Reversed queue : " + q);
   }
 }
