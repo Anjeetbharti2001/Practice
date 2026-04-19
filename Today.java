4ee15f7a9ca70a9d65d04ad4a74185ee5cee431f
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-19 08:04:12 +0530

    print student Registration

--- Today.java
+++ Today.java
@@ -1,9 +1,15 @@
 public class Today{
   public static void main(String args[]){
-     final int  myName = 30;
-     myName = 30; // This is output a error 
-     System.out.println(myName);
+    String studentName = "Anjeet";
+    int studentId = 045;
+    int studentage = 25;
+    float studentFee = 7.50f;
+    int studentGrade = 'B';
 
-
+    System.out.println("Student of Name :-" + studentName);
+    System.out.println("student of Id :- " + studentId);
+    System.out.println("Student of age :- " + studentage);
+    System.out.println("Student of fee :-" + studentFee);
+    System.out.println("Student of Grade : -" + studentGrade);
   }
 }
