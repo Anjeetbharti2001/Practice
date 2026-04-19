1d0baff7da58a8b1fd6445c98ef3e02f27e3e0a7
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-19 08:16:56 +0530

    Total Area

--- Today.java
+++ Today.java
@@ -1,15 +1,10 @@
 public class Today{
   public static void main(String args[]){
-    String studentName = "Anjeet";
-    int studentId = 045;
-    int studentage = 25;
-    float studentFee = 7.50f;
-    int studentGrade = 'B';
-
-    System.out.println("Student of Name :-" + studentName);
-    System.out.println("student of Id :- " + studentId);
-    System.out.println("Student of age :- " + studentage);
-    System.out.println("Student of fee :-" + studentFee);
-    System.out.println("Student of Grade : -" + studentGrade);
+    int length = 50;
+    int width = 5;
+    int Area = length*width;
+    System.out.println("Total of length : - " + length);
+    System.out.println("Total of Width : - " + width);
+    System.out.println("Total of Area :-" + Area);
   }
 }
