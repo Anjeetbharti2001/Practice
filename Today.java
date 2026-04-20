7692aff068da6a33b1f00f9f3d6e98970f67365c
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-20 09:21:31 +0530

    count even number in arrays

--- Today.java
+++ Today.java
@@ -1,11 +1,13 @@
 public class Today{
   public static void main(String args[]){
-    int arr[] = {3,5,6,7,7,8,9};
-    int sum = 0;
+    int arr[] = {3,5,7,8,9};
+    int count = 0;
 
     for(int i = 0; i<arr.length; i++){
-      sum += arr[i];
+       if(arr[i] %2 == 0){
+         count++;
+       }
     }
-    System.out.println("Total sum of arrays :-" + sum);
+    System.out.println(count);
   }
 }
