e94f207f45d3d944181220b832f6b9aee2c8b2d6
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-20 09:43:25 +0530

    maximum element in arrays

--- Today.java
+++ Today.java
@@ -1,13 +1,13 @@
 public class Today{
   public static void main(String args[]){
-    int arr[] = {4,5,6,7,9,9};
-    int min = arr[0];
+    int arr[] = {8,4,5,6,7,8};
+    int max = arr[0];
 
     for(int i = 0; i<arr.length; i++){
-      if(arr[i] < min){
-        min = arr[i];
+      if(arr[i] > max){
+        max = arr[i];
       }
     }
-    System.out.println(min);
+    System.out.println(max);
   }
 }
