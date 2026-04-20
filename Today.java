0bb9c1031a55f848e4d53d7c3163c849f3f4bf4b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-20 09:26:11 +0530

    count odd  number in arrays

--- Today.java
+++ Today.java
@@ -1,12 +1,12 @@
 public class Today{
   public static void main(String args[]){
-    int arr[] = {3,5,7,8,9};
-    int count = 0;
+    int arr[] = {3,5,66,7,8,9};
+    int count = 0; 
 
     for(int i = 0; i<arr.length; i++){
-       if(arr[i] %2 == 0){
-         count++;
-       }
+      if(arr[i] % 2 == 0){
+        count++;
+      }
     }
     System.out.println(count);
   }
