cbabc10be0783dc0e72216ba631cb28f5a05e688
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-22 09:43:23 +0530

    check stack emepty or not

--- Today.java
+++ Today.java
@@ -1,12 +1,12 @@
-// Peek operation (top element ko dekhna)
 import java.util.*;
 public class Today{
     public static void main(String args[]){
         Stack<Integer> s = new Stack<>();
-        s.push(2);
-        s.push(3);
+        
+        System.out.println(s.isEmpty());//true
 
-        System.out.println(s.peek());
+        s.push(10);
+        System.out.println(s.isEmpty());// False
 
     }
 }
