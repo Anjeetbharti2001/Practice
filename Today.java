8a5794a7172d7c8cb205e80b6b569b9c6e161a77
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-22 09:37:45 +0530

    top element see through stack

--- Today.java
+++ Today.java
@@ -1,14 +1,12 @@
-// Remove element in stack 
+// Peek operation (top element ko dekhna)
 import java.util.*;
 public class Today{
     public static void main(String args[]){
         Stack<Integer> s = new Stack<>();
+        s.push(2);
+        s.push(3);
 
-        s.push(10);
-        s.push(12);
-        s.push(90);
+        System.out.println(s.peek());
 
-        System.out.println(s.pop()); // remove top
-        System.out.println(s);
     }
 }
