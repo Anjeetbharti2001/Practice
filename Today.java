b4a8d173c52a5d268e2c33027c248c5c7cb8aa19
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-23 13:24:40 +0530

    toLowercase

--- Today.java
+++ Today.java
@@ -1,22 +1,6 @@
-import java.util.*;
 public class Today{
     public static void main(String args[]){
-        Queue<Integer> q = new LinkedList<>();
-        Stack<Integer> Stack = new Stack<>();
-
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        
-        //move queue to stack 
-
-        while (!q. isEmpty()){
-            Stack.push(q.remove());
-        }
-        //moves back to queue
-        while(!Stack.isEmpty()){
-            q.add(Stack.pop());
-        }
-        System.out.println("Removes :-" + q);
+        String str = "Anjeet bharti";
+        System.out.println(str.toLowerCase());
     }
 }
