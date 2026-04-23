db52f3e990100ca049b326766d5ad07bd125c1ca
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-23 12:53:23 +0530

    check queue empty yes or not

--- Today.java
+++ Today.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,5,66,7,8,5};
+        Queue<Integer> q = new LinkedList<>();
 
-        int len = arr.length;
-        System.out.println("Length of arrays = " + len);
+        q.add(10);
+        q.add(20);
+        q.add(30);
+
+        System.out.println("queue:" + q);
+        System.out.println("System" + q.poll());
+        System.out.println("front " + q.peek());
     }
 }
