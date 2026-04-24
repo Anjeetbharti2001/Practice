ff64326239e0fde17d03dced60caf9efb4fe317a
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-24 12:54:58 +0530

    total sum of arrays

--- Today.java
+++ Today.java
@@ -1,13 +1,8 @@
 public class Today{
     public static void main(String args[]){
-        int arr[] = {3,5,6,7,8};
-        int count = 0;
-
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] %2 == 0){
-                count++;
-            }
-        }
-        System.out.println(count);
+        int a = 3;
+        int b = 8;
+        int sum = a + b;
+        System.out.println("total sum :-" + sum);
     }
 }
