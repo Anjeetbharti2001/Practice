24bc072c0071903cbafe13533405da15a599288c
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-25 10:31:37 +0530

    Removes all items from the list

--- Today.java
+++ Today.java
@@ -1,22 +1,17 @@
+// Remove all items from the list
 import java.util.*;
+
 public class Today{
-
     public static void main(String args[]){
-        // add a collection of items to the list
-       ArrayList<String> boys = new ArrayList<String>();
-       boys.add("Anjeet");
-       boys.add("Bharti");
-       boys.add("Hanu");
-       boys.add("Veer");
-       boys.add("chita");
+        ArrayList<String> boys = new ArrayList<String>();
+        boys.add("Anjeet");
+        boys.add("bharti");
+        boys.add("Hanu");
+        boys.add("veer");
+        boys.add("Chita");
+        boys.add("sher");
+        boys.add("Tiger");
 
-       ArrayList<String> brands = new ArrayList<String>();
-       brands.add("Microsoft");
-       brands.add("Apple");
-       brands.add("Amazone");
-
-       brands.addAll(boys);
-
-       System.out.println(brands);
+        System.out.println(boys);
     }
 }
