5b9c584591d43cb4010359339db543f8e71ad68e
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-25 10:21:35 +0530

    add a collection of a iteam to the list

--- Today.java
+++ Today.java
@@ -1,12 +1,22 @@
 import java.util.*;
 public class Today{
-    public static void main(String args[]){
-        LinkedList<String> boys = new LinkedList<String>();
-        boys.add("Anjeet");
-        boys.add("Bharti");
-        boys.add("Hamu");
-        boys.add("Mazda");
 
-        System.out.println(boys);
+    public static void main(String args[]){
+        // add a collection of items to the list
+       ArrayList<String> boys = new ArrayList<String>();
+       boys.add("Anjeet");
+       boys.add("Bharti");
+       boys.add("Hanu");
+       boys.add("Veer");
+       boys.add("chita");
+
+       ArrayList<String> brands = new ArrayList<String>();
+       brands.add("Microsoft");
+       brands.add("Apple");
+       brands.add("Amazone");
+
+       brands.addAll(boys);
+
+       System.out.println(brands);
     }
 }
