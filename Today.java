668e05f523d959f6a7efe630467b3756b0c6fdad
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-25 11:08:48 +0530

    recursive

--- Today.java
+++ Today.java
@@ -1,20 +1,13 @@
-import java.util.ArrayList;
-
 public class Today{
+    public static int sum(int k){
+        if(k > 0){
+            return k + sum(k - 1);
+        }else{
+            return 0;
+        }
+    }
     public static void main(String args[]){
-        // checks whether an item exist in the list
-        ArrayList<String> boys = new ArrayList<String>();
-        boys.add("Anjeet");
-        boys.add("bharti");
-        boys.add("Hanu");
-        boys.add("veer");
-        boys.add("Chita");
-        boys.add("sher");
-        boys.add("Tiger");
-        boys.add("I am a problem solver");
-        
-        System.out.println(boys.contains("I am a problem solver"));
-
-        System.out.println(boys.contains("I am best software engineer"));
+       int result = sum(10);
+       System.out.println(result);
     }
 }
