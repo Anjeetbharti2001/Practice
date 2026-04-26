094638777e94904244a895a530546e6e8d8731ea
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-26 10:24:46 +0530

    count array element even number

--- Today.java
+++ Today.java
@@ -1,31 +1,13 @@
-// Two- Pointer (Manual Binary addition)
 public class Today{
-    public static String  addBinary(String x, String y){
-        int i = x.length() - 1;
-        int j = y.length() - 1;
-        int carry = 0;
-        StringBuilder result = new StringBuilder();
-
-        while(i >= 0 || j >= 0){
-            int sum = carry;
-         
-            if(i >= 0) sum += x.charAt(i--) - '0';
-            if(j >= 0) sum += y.charAt(j--) - '0';
-
-            result.append(sum % 2);
-            carry = sum / 2;
-        }
-        if(carry == 1){
-            result.append(1);
-        }
-        return result.reverse().toString();
-             
-
-    }
     public static void main(String args[]){
-      String x = "011011";
-      String y = "1010111";
+        int arr[] = {3,5,7,8};
+        int count = 0; 
 
-      System.out.println(addBinary(x, y));
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] %2 == 0){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
