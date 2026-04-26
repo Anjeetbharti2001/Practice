d41a32fe6862e9ada8a905f479eb260b5b5da1f9
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-26 10:18:08 +0530

    Two - pointer (manual Binary addtion)

--- Today.java
+++ Today.java
@@ -1,15 +1,31 @@
+// Two- Pointer (Manual Binary addition)
 public class Today{
-    static String addBinary(String x, String y){
+    public static String  addBinary(String x, String y){
+        int i = x.length() - 1;
+        int j = y.length() - 1;
+        int carry = 0;
+        StringBuilder result = new StringBuilder();
 
-        int num1 = Integer.parseInt(x, 2);
-        int num2 = Integer.parseInt(y, 2);
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+         
+            if(i >= 0) sum += x.charAt(i--) - '0';
+            if(j >= 0) sum += y.charAt(j--) - '0';
 
-        int sum = num1 + num2;
-        return Integer.toBinaryString(sum);
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+        if(carry == 1){
+            result.append(1);
+        }
+        return result.reverse().toString();
+             
+
     }
-    public static void main(String[] args){
-        String x = "011011";
-        String y = "1010111";
-        System.out.println(addBinary(x, y));
+    public static void main(String args[]){
+      String x = "011011";
+      String y = "1010111";
+
+      System.out.println(addBinary(x, y));
     }
 }
