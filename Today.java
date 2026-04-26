f25e4959640328291b02f1c21660ad7aaf00976b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-26 09:52:38 +0530

    add integer binary

--- Today.java
+++ Today.java
@@ -1,11 +1,15 @@
-// check Even or Odd
 public class Today{
-    public static void main(String args[]){
-        int number = 7; // You can also takes as user input
-        if(number % 2 == 0){
-            System.out.println(number + " is even.");
-        }else{
-            System.out.println(number + " is Odd.");
-        }
+    static String addBinary(String x, String y){
+
+        int num1 = Integer.parseInt(x, 2);
+        int num2 = Integer.parseInt(y, 2);
+
+        int sum = num1 + num2;
+        return Integer.toBinaryString(sum);
+    }
+    public static void main(String[] args){
+        String x = "011011";
+        String y = "1010111";
+        System.out.println(addBinary(x, y));
     }
 }
