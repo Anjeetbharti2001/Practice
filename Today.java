dd4af8cc234b8ff01f89f4c6a4b391c29f7620be
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-27 09:09:08 +0530

    collection framework in java

--- Today.java
+++ Today.java
@@ -1,20 +1,13 @@
 import java.util.*;
 public class Today{
-    //Write a program to insert 5 elements in a queue and print them
-
     public static void main(String args[]){
         Queue<Integer> q = new LinkedList<>();
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter five Element");
-        for(int i = 0; i< 5; i++){
-            q.add(sc.nextInt());
-        }
-        System.out.println("Queue element");
-        for(int x : q){
-            System.out.print(x + " ");
-        }
-
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        System.out.println("queue:-" + q);
+        System.out.println("remove" + q.poll());
+        System.out.println("front:" + q.peek());
     }
 }
