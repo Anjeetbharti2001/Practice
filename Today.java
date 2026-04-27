c3606bfa30bf49fac3ff0dadaeda7d7e8278f3ef
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-27 08:39:35 +0530

    Queue is not empty

--- Today.java
+++ Today.java
@@ -1,14 +1,13 @@
 import java.util.*;
 public class Today{
     public static void main(String args[]){
-        // Write a program to find the size of a queue.
         Queue<Integer> q = new LinkedList<>();
-
-        q.add(1);
-        q.add(2);
         q.add(3);
-        q.add(2);
 
-        System.out.println("Size of Queue :- " + q.size());
+        if(q.isEmpty()){
+            System.out.println("Queue is Empty:-");
+        }else{
+            System.out.println("Queue is not empty");
+        }
     }
 }
