0bce31dddb9ffb5038c5a3fbbf3806d75606552d
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-27 08:31:49 +0530

    find the size of a queue

--- Today.java
+++ Today.java
@@ -1,28 +1,14 @@
 import java.util.*;
-
 public class Today{
-    // Write a java program to reversed a queue
-
     public static void main(String args[]){
+        // Write a program to find the size of a queue.
         Queue<Integer> q = new LinkedList<>();
-
-        Stack <Integer> Stack = new Stack<>();
 
         q.add(1);
         q.add(2);
         q.add(3);
+        q.add(2);
 
-        //move to stack
-
-        while(!q.isEmpty()){
-            Stack.push(q.remove());
-        }
-
-        //move back to queue
-        while(!Stack.isEmpty()){
-            q.add(Stack.pop());
-        }
-        System.out.println("Reversed:-" + q);
-
+        System.out.println("Size of Queue :- " + q.size());
     }
 }
