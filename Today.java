befdaa5a07abd5adc44c4392d48471cd8cc221cb
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-27 08:05:15 +0530

    reversed a queue

--- Today.java
+++ Today.java
@@ -1,9 +1,28 @@
-public class Today{
-    public static void main(String args[]){
-        int a = 91;
-        int b = 2;
+import java.util.*;
 
-        int sum = a%b;
-        System.out.println(sum);
+public class Today{
+    // Write a java program to reversed a queue
+
+    public static void main(String args[]){
+        Queue<Integer> q = new LinkedList<>();
+
+        Stack <Integer> Stack = new Stack<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        //move to stack
+
+        while(!q.isEmpty()){
+            Stack.push(q.remove());
+        }
+
+        //move back to queue
+        while(!Stack.isEmpty()){
+            q.add(Stack.pop());
+        }
+        System.out.println("Reversed:-" + q);
+
     }
 }
