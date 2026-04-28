2fdec49dea437ea312d4509b8f527d1b63fa9da3
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-28 08:33:57 +0530

    print number pattern in a triangle shape

--- Today.java
+++ Today.java
@@ -1,8 +1,8 @@
 public class Today{
-    //java program to print a number pattern in a reverse triangle shape.
+    //java programming to print a number pattern in a triangle shape.
     public static void main(String args[]){
-        for(int i = 5; i>=1 ; i--){
-            for(int j = 1; j<= i; j++){
+        for(int i = 1; i<= 5; i++){
+            for(int j = 1; j<=i; j++){
                 System.out.print(i);
             }
             System.out.println();
