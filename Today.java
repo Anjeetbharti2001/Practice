e91aff31ccc9fecd171a732ff05bf235276d9635
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-28 08:41:26 +0530

    print star pattern reverse triangle shape

--- Today.java
+++ Today.java
@@ -1,9 +1,9 @@
 public class Today{
-    //java programming to print a number pattern in a triangle shape.
+    // java Programming to print star pattern in a reverse triangle shape.
     public static void main(String args[]){
-        for(int i = 1; i<= 5; i++){
+        for(int i = 5; i>=1; i--){
             for(int j = 1; j<=i; j++){
-                System.out.print(i);
+                System.out.print("* ");
             }
             System.out.println();
         }
