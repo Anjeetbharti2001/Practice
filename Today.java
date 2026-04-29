494840b5ae10ad7c3c75d397e9bb04c3c0e735a2
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-29 17:21:02 +0530

    addition and subtraction operators

--- Today.java
+++ Today.java
@@ -1,9 +1,13 @@
 public class Today{
     public static void main(String args[]){
-        int a = 2;
-        int b = 5;
+        int a = 9; int b = 8;
+        // addition 
         int sum = a + b;
+        
 
+        // subtraction
+        int diff = a- b;
         System.out.println(sum);
+        System.out.println("Subtraction Integer: -" + diff);
     }
 }
