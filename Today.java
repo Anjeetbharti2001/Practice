9c42fecb119b2a57782a695bee297a17103af532
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-29 17:07:55 +0530

    sum of two integer value

--- Today.java
+++ Today.java
@@ -1,12 +1,9 @@
 public class Today{
     public static void main(String args[]){
-        int limit = 50;
-        System.out.println("Printing Even number between 1 and " + limit);
-        for(int i = 1; i<= limit; i++){
-            // if the number is divisible by 2 then it is even 
-            if(i % 2 == 0){
-                System.out.print(i+ " ");
-            }
-        }
+        int a = 2;
+        int b = 5;
+        int sum = a + b;
+
+        System.out.println(sum);
     }
 }
