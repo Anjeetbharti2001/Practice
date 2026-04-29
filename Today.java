9b94ce8b2699dd1e7c2579aa46bdfa9315391f6a
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-29 17:27:03 +0530

    multiplication and Division

--- Today.java
+++ Today.java
@@ -1,13 +1,13 @@
 public class Today{
     public static void main(String args[]){
-        int a = 9; int b = 8;
-        // addition 
-        int sum = a + b;
-        
+        // Multiplication 
+        int a = 4; int b = 3;
+        int mul = a * b;
 
-        // subtraction
-        int diff = a- b;
-        System.out.println(sum);
-        System.out.println("Subtraction Integer: -" + diff);
+        // Division 
+        int div = a / b;
+
+        System.out.println("Total division value:-" + div);
+        System.out.println("Total multiplication :-" + mul);
     }
 }
