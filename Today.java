f913f8173aec5ed0f6c6dcd290d32fc3430421d6
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-29 17:48:00 +0530

    print even total value

--- Today.java
+++ Today.java
@@ -1,11 +1,13 @@
 public class Today{
     public static void main(String args[]){
-        int b = 10;
-       
+        int arr[] = {4,6,7,8,9};
+        int count = 0;
 
-        // using unary operators
-        System.out.println("Post Decreament :- " + (b--));
-        System.out.println("Pre decrement:-" + (--b));
-
+        for(int i = 0 ;i<arr.length; i++){
+            if(arr[i] %2 == 0){
+                count++;
+            }
+        }
+        System.out.println(" Total even number:-" + count);
     }
 }
