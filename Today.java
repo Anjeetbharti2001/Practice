1f24a6943d9b9da9e32ba69b2988cd51c813935c
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-04-30 07:48:25 +0530

    Ignore Duplicate value in HashSet

--- Today.java
+++ Today.java
@@ -1,13 +1,12 @@
+import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int arr[] = {4,6,7,8,9};
-        int count = 0;
+        HashSet<String> set = new HashSet<>();
 
-        for(int i = 0 ;i<arr.length; i++){
-            if(arr[i] %2 == 0){
-                count++;
-            }
-        }
-        System.out.println(" Total even number:-" + count);
+        set.add("Anjeet");
+        set.add("Sarjun");
+        set.add("Anjeet"); // Dplicate innore
+
+        System.out.println(set);
     }
 }
