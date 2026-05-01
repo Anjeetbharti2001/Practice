e3e45932ebdcd1428d06196c9c7df49eed2b3f2b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-01 11:25:14 +0530

    create a tree and Print root value

--- Today.java
+++ Today.java
@@ -1,21 +1,21 @@
+// Create a boinary tree and print the root
+
+// Question: Create a tree and print root value
+
 class Node{
     int data;
     Node left, right;
 
     Node(int value){
         data = value;
+
         left = right = null;
     }
 }
-
 public class Today{
     public static void main(String args[]){
-        Node root = new Node(1);// root
-        root.left = new Node(2);
-        root.right = new Node(3);
-        root.left.left = new Node(4);
-        root.left.right = new Node(5);
+        Node root = new Node(10);
 
-        System.out.println("Tree Create Succsessfully");
+        System.out.println("root: " + root.data);
     }
 }
