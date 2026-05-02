f454740db67dd1136483fcdf953d0540f188becd
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-02 08:59:53 +0530

    New String Builder in java

--- Today.java
+++ Today.java
@@ -1,13 +1,12 @@
-//Sorting argorithm
-import java.util.*;
 public class Today{
     public static void main(String args[]){
-        // Arrays example
-        int [] nums = {5,3,8,1};
-        Arrays.sort(nums);
-        System.out.println("Sorted arrays:-" + Arrays.toString(nums));
-        List<Integer>list = new ArrayList<>(Arrays.asList(5,3,8,1));
-        Collections.sort(list);
-        System.out.println("Sorted list: " + list);
+        //Immutable String 
+        String s = "Hello Anjeet beta";
+        System.out.println(s);
+
+        //Mutable String builder
+        StringBuilder sb = new StringBuilder("Hello" + " ");
+        sb.append("Anjeet");
+        System.out.println(sb.toString());
     }
 }
