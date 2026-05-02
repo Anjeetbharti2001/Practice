d76d86c409a8b05b20fe0f3bf45f46c2cb791b17
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-02 09:18:30 +0530

    Recursion

--- Today.java
+++ Today.java
@@ -1,12 +1,9 @@
-import java.util.*;
 public class Today{
+    static int fact(int n){
+        if(n == 0) return 1;
+        return n  * fact(n -1);
+    }
     public static void main(String args[]){
-        Map<Object, Object> map = new HashMap<>();
-        map.put(10, "Anjeet");
-        map.put(20, "hello");
-        map.put("hello", "world");
-        map.put(2.0, 55);
-
-        System.out.println(map);
+         System.out.println(fact(5));
     }
 }
