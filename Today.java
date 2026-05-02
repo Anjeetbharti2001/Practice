d2a44496fdb6f0b7e099f49eed8ada3cc4791fa2
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-02 08:53:46 +0530

    sorting argorithms

--- Today.java
+++ Today.java
@@ -1,17 +1,13 @@
+//Sorting argorithm
 import java.util.*;
 public class Today{
     public static void main(String args[]){
-       List<Integer> List = Arrays.asList(2,4,6,8,10);
-       int key = 6;
-       // Linear Search 
-       System.out.println("Linear Search : -" + List.contains(key));
-       // Binary Search using the collections.BinarySearch
-       //(list must be sorted)
-       int index = Collections.binarySearch(List,key);
-       if(index >= 0){
-        System.out.println("Element Found at index " + index);
-       }else{
-        System.out.println("Element not Found");
-       }
+        // Arrays example
+        int [] nums = {5,3,8,1};
+        Arrays.sort(nums);
+        System.out.println("Sorted arrays:-" + Arrays.toString(nums));
+        List<Integer>list = new ArrayList<>(Arrays.asList(5,3,8,1));
+        Collections.sort(list);
+        System.out.println("Sorted list: " + list);
     }
 }
