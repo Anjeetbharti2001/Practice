e4aa89c7308eb983fab388ceb05325a1f2547dfd
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-03 09:44:06 +0530

    pointer c practice

--- hello.c
+++ hello.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
+void disp(int*p){
+    printf("%d",*p);
+    printf("%d",*(p+3));
+}
 int main(){
-    printf("Anjeet bharti");
+    int a[] = {11,2,3,4,5};
+    {
+        disp(&a);
+    }
+    return 0;
 }
