b14917488e00307c7b1f4ad4312484206b57531f
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-04 09:21:58 +0530

    merge element using heap

--- Today.java
+++ Today.java
@@ -1,14 +1,16 @@
-import java.util.*;
+// Merge two Element Using Heap
 
+import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int[] arr = {4,2,7,1,3};
+        int arr[] = {1,4,7};
+        int arr1[] = {2,3,6};
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        for(int num : arr){
-            pq.add(num);
-        }
+        for(int num : arr) pq.add(num);
+        for(int num : arr1) pq.add(num);
+
         while(!pq.isEmpty()){
             System.out.print(pq.poll() + " ");
         }
