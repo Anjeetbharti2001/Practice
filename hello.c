4f483c7a83c1ce02e6bd4ff2c3c054006e868f0c
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-04 11:41:52 +0530

    Write a function to add two number

--- hello.c
+++ hello.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
-void disp(int*p){
-    printf("%d",*p);
-    printf("%d",*(p+3));
+// Write a function to add ntwo number 
+#include <stdio.h>
+
+int add(int a, int b){
+    return a + b;
 }
+
 int main(){
-    int a[] = {11,2,3,4,5};
-    {
-        disp(&a);
-    }
+    int result = add(5,3);
+    printf("sum = %d", result);
     return 0;
 }
