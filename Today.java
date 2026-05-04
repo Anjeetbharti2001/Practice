d97987091c84f94bda06c5080c4cac3a79796b52
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-04 09:05:20 +0530

    Find Largest Element

--- Today.java
+++ Today.java
@@ -1,23 +1,14 @@
-// Find Kth Smallest Element 
-
 import java.util.*;
 
 public class Today{
     public static void main(String args[]){
-        int arr[] = {7,10,4,3,20,15};
-
-        int k = 2;
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int arr[] = {5,3,9,1,6};
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
         for(int num : arr){
             pq.add(num);
         }
 
-        for(int i = 0; i< k; i++){
-            pq.poll();
-        }
-
-        System.out.println(pq.peek());
+        System.out.println("Largest : " + pq.peek());
     }
 }
