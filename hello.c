9ad9ab684b117ebd071d7d95a9779627c6ab9dd0
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-04 11:50:56 +0530

    FUNCTION WITHOUT RETURN VALUE(VOID)

--- hello.c
+++ hello.c
@@ -1,12 +1,10 @@
-// Write a function to add ntwo number 
-#include <stdio.h>
+// 
+#include<stdio.h>
 
-int add(int a, int b){
-    return a + b;
+void greet(){
+    printf("Hello Anjeet");
 }
-
 int main(){
-    int result = add(5,3);
-    printf("sum = %d", result);
+    greet();
     return 0;
 }
