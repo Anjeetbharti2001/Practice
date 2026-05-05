5d4ba5d51774d311f0bcf6a13bced125a552ad12
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-05 09:52:54 +0530

    Climbing stairs

--- Today.java
+++ Today.java
@@ -1,17 +1,18 @@
+// You can climb 1 or 2 steps. Find total ways to rach n stairs.
 public class Today{
-    public static int fib(int n){
-        if(n <= 1) return n;
+    public static int ways(int n ){
+        if(n <= 2) return n;
 
-        int dp[] = new int[n + 1];
-        dp[0] = 0;
+        int dp[] = new  int[n + 1];
         dp[1] = 1;
+        dp[2] = 2;
 
-        for(int i = 2; i<= n; i++){
-            dp[i] = dp[i -1] + dp[i - 2];
+        for(int i = 3; i<= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
     }
     public static void main(String args[]){
-       System.out.println(fib(6));
+       System.out.println(ways(4));// 5
     }
 }
