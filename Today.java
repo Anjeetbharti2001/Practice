dad72ee403672f2943531224b4f5a04889acf56d
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-06 09:52:36 +0530

    convert string to uppercase

--- Today.java
+++ Today.java
@@ -1,15 +1,11 @@
-// Count vovels in String 
+// count string to uppercase 
+
 public class Today{
     public static void main(String args[]){
-        String str = "education";
-        int count = 0; 
+        String str = "Hello world";
 
-        for(int i = 0; i<str.length(); i++){
-            char ch = str.charAt(i);
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-                count++;
-            }
-        }
-        System.out.println("Vowels : " + count);
+        String upper = str.toUpperCase();
+
+        System.out.println("UpperCase :" + upper);
     }
 }
