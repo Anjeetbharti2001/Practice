53acd59ec84fa7b2f9ca831d93db8d84cb5d374c
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-06 10:11:38 +0530

    check stack is empty

--- Today.java
+++ Today.java
@@ -1,15 +1,14 @@
-//pop element from stack
+// Peek Top element 
 import java.util.*;
+
 public class Today{
     public static void main(String args[]){
         Stack<Integer> stack = new Stack<>();
 
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
+        stack.push(5);
+        stack.push(15);
+        stack.push(25);
 
-        stack.pop();
-
-        System.out.println(stack);
+        System.out.println("Top element : - " + stack.peek());
     }
 }
