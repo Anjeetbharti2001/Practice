41cfa5b0b0ce3ae8851d63ff395b1011c37294b6
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-06 10:06:42 +0530

    Pop element from stack

--- Today.java
+++ Today.java
@@ -1,3 +1,4 @@
+//pop element from stack
 import java.util.*;
 public class Today{
     public static void main(String args[]){
@@ -6,6 +7,8 @@ public class Today{
         stack.push(10);
         stack.push(20);
         stack.push(30);
+
+        stack.pop();
 
         System.out.println(stack);
     }
