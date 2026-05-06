ad07f993b3c1b725ea59325460ac64928886aacb
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-06 11:34:04 +0530

    Preorder Traversal

--- Today.java
+++ Today.java
@@ -1,30 +1,26 @@
-// Indorder Traversal(LEFT, Root, Right)
 class Node {
     int data;
     Node left, right;
 
     Node(int data){
         this.data = data;
-        left = right = null;
     }
 }
 
 
-
 public class Today{
-    public static void inorder(Node root){
+    public static void preorder(Node root){
         if(root != null){
-            inorder(root.left);
             System.out.print(root.data + " ");
-            inorder(root.right);
-
+            preorder(root.left);
+            preorder(root.right);
         }
     }
     public static void main(String args[]){
-        Node root = new Node(10);
-        root.left = new Node(20);
-        root.right = new Node(30);
+       Node root = new Node(10);
+       root.left = new Node(20);
+       root.right = new Node(30);
 
-        inorder(root);
+       preorder(root);
     }
 }
