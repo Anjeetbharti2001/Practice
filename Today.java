2601673141f0146c50af140f4775e210f53dc78a
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-06 11:24:04 +0530

    Inorder Traversal

--- Today.java
+++ Today.java
@@ -1,5 +1,5 @@
-// create a simple binary tree 
-class Node{
+// Indorder Traversal(LEFT, Root, Right)
+class Node {
     int data;
     Node left, right;
 
@@ -9,12 +9,22 @@ class Node{
     }
 }
 
+
+
 public class Today{
+    public static void inorder(Node root){
+        if(root != null){
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+
+        }
+    }
     public static void main(String args[]){
         Node root = new Node(10);
         root.left = new Node(20);
         root.right = new Node(30);
 
-        System.out.println("Root : " + root.data);
+        inorder(root);
     }
 }
