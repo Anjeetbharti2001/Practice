19d685809fc2ec5e352da71967747427ed5d42ec
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-08 08:20:36 +0530

    Find length of string

--- hello.c
+++ hello.c
@@ -1,10 +1,25 @@
-//Print Anjeet using library function 
+// #include<stdio.h>
+
+// int main(){
+//     int num;
+
+//     scanf("%d", &num);
+
+//     prinf("Number = %d", num);
+
+//     return 0;
+// }
+
+// find lenght of string 
 
 #include<stdio.h>
+#include<string.h>
 
 int main(){
-    printf("Hello world show the output ");
+    char name[] = "Anjeet";
+    int len = strlen(name);
+
+    printf("Length = %d", len);
 
     return 0;
-
 }
