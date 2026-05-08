4d108ed562747b8cbfbe91142c9663050afa552d
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-08 17:56:23 +0530

    adjacent List

--- Today.java
+++ Today.java
@@ -1,16 +1,22 @@
+import java.util.*;
 public class Today{
     public static void main(String args[]){
-        String str = "jahaj";
-        String rev = "";
 
-        for(int i = str.length() - 1; i>= 0 ; i--){
-            rev += str.charAt(i);
+        int v = 4;
+
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for(int i = 0; i< v ; i++){
+            graph.add(new ArrayList<>());
         }
 
-        if(str.equals(rev)){
-            System.out.println("Palindrom");
-        }else{
-            System.out.println("Not palindrom");
-        }
+        // Edge add
+        graph.get(0).add(1);
+        graph.get(1).add(0);
+
+        graph.get(0).add(2);
+        graph.get(2).add(0);
+
+        System.out.println(graph);
     }
 }
