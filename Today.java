f40f37d381b92ba6fcdfb7147b12a9c58ffa252f
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-09 10:43:45 +0530

    push three number in stack and print them in reversed order

--- Today.java
+++ Today.java
@@ -1,22 +1,14 @@
 import java.util.*;
 public class Today{
     public static void main(String args[]){
+        Stack<Integer> st = new Stack<>();
 
-        int v = 4;
+        st.push(10);
+        st.push(20);
+        st.push(30);
 
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-
-        for(int i = 0; i< v ; i++){
-            graph.add(new ArrayList<>());
+        while(!st.isEmpty()){
+            System.out.println(st.pop());
         }
-
-        // Edge add
-        graph.get(0).add(1);
-        graph.get(1).add(0);
-
-        graph.get(0).add(2);
-        graph.get(2).add(0);
-
-        System.out.println(graph);
     }
 }
