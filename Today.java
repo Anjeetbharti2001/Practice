b20cacef37c9a464851f3fb7cbdf796fd3bdee67
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-11 10:16:04 +0530

    check String Contains Word

--- Today.java
+++ Today.java
@@ -1,61 +1,7 @@
-// //String length
-// public class Today{
-//     public static void main(String args[]){
-//         String name = "Anjeet";
-
-//         System.out.println(name.length());
-//     }
-// }
-
-// Convert string to uppercase
-// public class Today{
-//     public static void main(String args[]){
-//         String city = "bhubaneswer";
-
-//         System.out.println(city.toUpperCase());
-//     }
-// }
-
-//convert string to lowercase
-
-// public class Today{
-//     public static void main(String args[]){
-//         String word = "HELLO";
-
-//         System.out.println(word.toLowerCase());
-//     }
-// }
-
-// Compare two String 
-
-// public class Today{
-//     public static void main(String args[]){
-//         String a = "java";
-//         String b = "java";
-
-//         System.out.println(a.equals(b));
-//     }
-// }
-
-// concatenate Two Strings
-
-// public class Today{
-//     public static void main(String args[]){
-//         String first = "Hello";
-//         String second = "world";
-
-//         String result = first + second;
-
-//         System.out.println(result);
-//     }
-// }
-
-// Find the character Using charAt
-
 public class Today{
     public static void main(String args[]){
-        String name = "Anjeet";
+        String sentence = "I love java";
 
-        System.out.println(name.charAt(2));
+        System.out.println(sentence.contains("java"));
     }
 }
