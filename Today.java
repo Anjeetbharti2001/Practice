7d9af733694b998a8bf214c6cacd43719d468b48
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-11 09:52:17 +0530

    String of length

--- Today.java
+++ Today.java
@@ -1,9 +1,8 @@
+//String length
 public class Today{
     public static void main(String args[]){
-        int n = 10;
-        while(n > 0){
-            n /= 2;
-            System.out.println(n * n);
-        }
+        String name = "Anjeet";
+
+        System.out.println(name.length());
     }
 }
