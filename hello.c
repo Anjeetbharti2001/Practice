871f18bc8aa544c7cbaaf38e2b5dfc22fe9226ed
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-12 10:47:59 +0530

    no argument but return value

--- hello.c
+++ hello.c
@@ -1,15 +1,22 @@
-// write a function without return values 
+// write a function no argument but return value
 
 #include<stdio.h>
-//function declaration
-void greet();
-int main(){
-    greet(); // function call
 
+//function declaration
+int add();
+
+//main function 
+int main(){
+    int result ;
+    result = add(); // calling function 
+    printf("sum = %d", result);
     return 0;
 }
+// function defination
+int add(){
+    int a = 10, b = 20;
+    int sum;
 
-// function defination 
-void greet(){
-    printf("hello. welcome to c programming ");
+    sum = a + b;
+    return sum ;
 }
