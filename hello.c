31d57e7db06a47bd3f54d91429a97d63753abe6e
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-12 10:38:37 +0530

    function without return value

--- hello.c
+++ hello.c
@@ -1,11 +1,15 @@
+// write a function without return values 
+
 #include<stdio.h>
-int add(int a , int b){
+//function declaration
+void greet();
+int main(){
+    greet(); // function call
+
     return 0;
 }
 
-int main(){
-    int num1 = 5, num2 = 10;
-    int result = add(num1, num2);
-    printf("sum = %d\n", result);
-    return 0;
+// function defination 
+void greet(){
+    printf("hello. welcome to c programming ");
 }
