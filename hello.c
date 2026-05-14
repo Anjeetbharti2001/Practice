2365c60f90d08a0d666529303318c9192fb7a33a
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-14 08:49:36 +0530

    Check Path Between Two nodes

--- hello.c
+++ hello.c
@@ -1,21 +1,30 @@
-// count number of edges 
-
 #include<stdio.h>
-int main(){
-    int graph[3][3] = {
-        {0,1,1},
-        {1,0,1},
-        {1,1,0}
-    };
-    int edges = 0;
 
-    for(int i = 0; i<3; i++){
-        for(int j = 0; j<3; j++){
-            if(graph[i][j] == 1)
-            edges++;
+int graph[4][4] = {
+    {0,1,1,0},
+    {1,0,1,1},
+    {1,1,0,0},
+    {0,1,0,0}
+};
+int visited[4] = {0};
+
+void DFS(int node){
+    visited[node] = 1;
+    for(int i = 0; i<4; i++){
+        if(graph[node][i] == 1 && !visited[i]){
+            DFS(i);
         }
     }
-    edges = edges / 2;
-    printf("Total Edges = %d", edges);
-    return 0;
+}
+int main(){
+    int start = 0;
+    int end = 3;
+
+    DFS(start);
+    if(visited[end])
+    printf("Path Exists");
+    else
+       printf("No Path");
+
+       return 0;
 }
