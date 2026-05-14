c1aaef4fed2461ba5e8df812387a8b9ae5ced6ce
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-14 08:20:11 +0530

    Breadth First Search

--- hello.c
+++ hello.c
@@ -1,18 +1,37 @@
-// create a graph out put display
+// BFS(Breadth first search)
+
 #include<stdio.h>
 
-int main(){
-    int graph[3][3] = {
-        {0,1,1},
-        {1,0,1},
-        {1,1,0}
-    };
-    printf("Adjacency Matrix:\n");
-    for(int i = 0; i< 3; i++){
-        for(int j = 0; j< 3; j++){
-            printf("%d", graph[i][j]);
+int graph[4][4] = {
+    {0,1,1,0},
+    {1,0,1,1},
+    {1,1,0,0},
+    {0,1,0,0}
+};
+
+int visited[4] = {0};
+void BFS(int start){
+    int queue[10];
+    int front = 0, rear = 0;
+
+    visited[start] = 1;
+    queue[rear++] = start;
+while(front < rear){
+    int current = queue[front++];
+    printf("%d", current);
+
+    for(int i = 0; i<4; i++){
+        if(graph[current][i] == 1 && !visited[i]){
+            visited[i] = 1;
+            queue[rear++] = i;
         }
-        printf("\n");
     }
+}
+
+}
+
+int main(){
+    printf("BFS Traversal : ");
+    BFS(0);
     return 0;
 }
