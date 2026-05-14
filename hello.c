c7fd103c5f6c9094f2d170981e6c3bc70f613f3a
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-14 08:03:20 +0530

    create a graph using c programming

--- hello.c
+++ hello.c
@@ -1,17 +1,18 @@
-// tail recursive function to calculate factorial 
+// create a graph out put display
 #include<stdio.h>
 
-unsigned int factTR(unsigned int n, unsigned int a){
-    if(n <= 1)
-    return a ;
-
-    return factTR(n - 1, n * a);
-}
-// A wrapper over factTR
-unsigned int fact(unsigned int n){
-    return factTR(n, 1);
-}
 int main(){
-    printf("%u", fact(5));
+    int graph[3][3] = {
+        {0,1,1},
+        {1,0,1},
+        {1,1,0}
+    };
+    printf("Adjacency Matrix:\n");
+    for(int i = 0; i< 3; i++){
+        for(int j = 0; j< 3; j++){
+            printf("%d", graph[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
