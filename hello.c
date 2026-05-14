8f0d1cbd9fbf5fe401e161b9008b63bb6152eed3
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-14 18:00:51 +0530

    simple pinter code in  c language

--- hello.c
+++ hello.c
@@ -1,30 +1,18 @@
 #include<stdio.h>
-
-int graph[4][4] = {
-    {0,1,1,0},
-    {1,0,1,1},
-    {1,1,0,0},
-    {0,1,0,0}
-};
-int visited[4] = {0};
-
-void DFS(int node){
-    visited[node] = 1;
-    for(int i = 0; i<4; i++){
-        if(graph[node][i] == 1 && !visited[i]){
-            DFS(i);
-        }
-    }
-}
 int main(){
-    int start = 0;
-    int end = 3;
+    int num = 10;
 
-    DFS(start);
-    if(visited[end])
-    printf("Path Exists");
-    else
-       printf("No Path");
+    // Pointer variable 
+    int*ptr;
 
-       return 0;
+    // Address store karna 
+    ptr = &num;
+
+    printf("Value of num = %d\n", num );
+    printf("Address of num = %p\n",&num);
+
+    printf("Pointer ptr store address = %p\n", ptr);
+    printf("value using pointer = %d\n", *ptr);
+
+    return 0;
 }
