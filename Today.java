d760be9b7c63005069e2499192a6f029b8283cd6
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-15 09:08:07 +0530

    factorial number

--- Today.java
+++ Today.java
@@ -1,14 +1,12 @@
 public class Today{
-    public static int sum(int n){
-        // base condition 
-        if(n == 1)
+    public static int fact(int n){
+        // BASE CONDITION
+        if(n == 0)
             return 1;
 
-        return n + sum(n - 1);
+        return n * fact(n -1);
     }
     public static void main(String args[]){
-        int n = 5; 
-        System.out.println(sum(n));
-
+        System.out.println("Factorial of  5 : " + fact(5));
     }
 }
