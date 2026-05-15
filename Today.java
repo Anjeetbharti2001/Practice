97fb5194ae5723d236d7c51061dbe2912e010b22
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-15 16:41:16 +0530

    Count total characters

--- Today.java
+++ Today.java
@@ -1,12 +1,7 @@
 public class Today{
-    public static int fact(int n){
-        // BASE CONDITION
-        if(n == 0)
-            return 1;
-
-        return n * fact(n -1);
-    }
     public static void main(String args[]){
-        System.out.println("Factorial of  5 : " + fact(5));
+        String str = " Programming";
+
+        System.out.println("Character = " + str.length());
     }
 }
