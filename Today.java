ea37f1c73cf756ad27edb591115d5e1b2cd1a072
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-16 07:04:08 +0530

    Find Duplicate Character

--- Today.java
+++ Today.java
@@ -1,9 +1,18 @@
 public class Today{
     public static void main(String args[]){
-        String str = "java Programming ";
+        String str = "programming";
 
-        str = str.replace(" ", "");
+        for(int i = 0; i<str.length(); i++){
+            int count = 1;
 
-        System.out.println(str);
+            for(int j = i + 1; j < str.length(); j++){
+                if(str.charAt(i) == str.charAt(j)){
+                    count++;
+                }
+            }
+            if(count > 1){
+                System.out.println(str.charAt(i));
+            }
+        }
     }
 }
