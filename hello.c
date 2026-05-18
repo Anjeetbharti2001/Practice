a7fcd8f98f5be5471c1a3dcd9f0e9fc033e3cfd8
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-18 08:35:12 +0530

    convert a Person's Name in Abbreviated

--- hello.c
+++ hello.c
@@ -1,13 +1,18 @@
-// Area of Triangle
+//convert a Person's Name in Abbreviated
 #include<stdio.h>
-
 int main(){
-    float base, height, area;
-    printf("Enter base and Height :-");
-    scanf("%f %f", &base, &height);
+    char fname[20], mname[20], lname[20];
 
-    area = 0.5 * base * height;
-    printf(" Area = %.2f", area);
+    printf("Enter first name : ");
+    scanf("%s", fname);
+
+    printf("Enter Middle Name : ");
+    scanf("%s", mname);
+
+    printf("Enter Last Name :");
+    scanf("%s", lname);
+
+    printf("Abbreviated Name: %c.%c.%s", fname[0], mname[0], lname);
 
     return 0;
 }
