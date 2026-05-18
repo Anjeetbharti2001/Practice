b684891799d8fd9596812f18113cfbe9485335eb
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-19 03:55:17 +0530

    print number from n to 1 using recursion

--- Today.java
+++ Today.java
@@ -1,12 +1,11 @@
 public class Today{
-    static void print1ToN(int n){
+    static void printNTo1(int n){
         if(n == 0)
             return;
-
-        print1ToN(n - 1);
         System.out.print(n + " ");
+        printNTo1(n - 1);
     }
     public static void main(String args[]){
-        print1ToN(5);
+        printNTo1(5);
     }
 }
