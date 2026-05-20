20141c2659bf0c41ad4d45088c59fea8a983db89
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-20 18:48:34 +0530

    Find Minimum number in Arrays

--- Today.java
+++ Today.java
@@ -1,14 +1,13 @@
+// find minimum number in arrays
 public class Today{
-    static void even(int n ){
-        if(n == 0)
-            return ;
+    static int min(int arr[], int n){
+        if(n == 1)
+            return arr[0];
 
-        even(n - 1);
-
-        if(n % 2 == 0)
-            System.out.println(n + " ");
+        return Math.min(arr[n - 1], min(arr, n - 1));
     }
     public static void main(String args[]){
-        even(10);
+        int arr[] = {5, 2, 8, 1, 9};
+        System.out.println(min(arr, arr.length));
     }
 }
