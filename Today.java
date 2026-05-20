f58f454ccc68537d29003712db883b098ac3897f
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-20 18:28:09 +0530

    Count Digit Using Recursion

--- Today.java
+++ Today.java
@@ -1,11 +1,11 @@
 public class Today{
-    static int max(int arr[], int n){
-        if(n == 1)
-            return arr[0];
-        return Math.max(arr[n - 1], max(arr, n -1));
+    static int countDigits(int n ){
+        if(n == 0)
+            return 0;
+
+        return 1 + countDigits(n /10);
     }
     public static void main(String args[]){
-        int arr[] = {2, 8, 1, 15, 6};
-       System.out.println(max(arr, arr.length));
+        System.out.println(countDigits(12345));
     }
 }
