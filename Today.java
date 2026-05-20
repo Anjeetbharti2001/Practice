40eefe8a72f36c4b1dc0acfc531e237a25685301
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-20 18:24:00 +0530

    Find Maximum Element In arrays Using Recursion

--- Today.java
+++ Today.java
@@ -1,23 +1,11 @@
-public class Today {
-
-    static boolean palindrome(String str, int start, int end) {
-
-        if (start >= end)
-            return true;
-
-        if (str.charAt(start) != str.charAt(end))
-            return false;
-
-        return palindrome(str, start + 1, end - 1);
+public class Today{
+    static int max(int arr[], int n){
+        if(n == 1)
+            return arr[0];
+        return Math.max(arr[n - 1], max(arr, n -1));
     }
-
-    public static void main(String args[]) {
-
-        String s = "madam";
-
-        if (palindrome(s, 0, s.length() - 1))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not palindrome");
+    public static void main(String args[]){
+        int arr[] = {2, 8, 1, 15, 6};
+       System.out.println(max(arr, arr.length));
     }
 }
