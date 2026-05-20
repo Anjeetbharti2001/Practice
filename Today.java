11328d4c3125ef2248b3d22fbe90d7d3c0e02b0e
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-20 18:32:29 +0530

    Find Sum of Digits Using Recursion

--- Today.java
+++ Today.java
@@ -1,11 +1,11 @@
 public class Today{
-    static int countDigits(int n ){
+    static int sumDigits(int n){
         if(n == 0)
             return 0;
 
-        return 1 + countDigits(n /10);
+        return (n % 10) + sumDigits(n /10);
     }
     public static void main(String args[]){
-        System.out.println(countDigits(12345));
+        System.out.println(sumDigits(1234));
     }
 }
