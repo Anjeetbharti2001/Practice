3b0be005e8f3911fc2eae0e3f5fcf6833aeeebe0
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-20 18:52:45 +0530

    Multiply Digits Of a Number

--- Today.java
+++ Today.java
@@ -1,13 +1,11 @@
-// find minimum number in arrays
 public class Today{
-    static int min(int arr[], int n){
-        if(n == 1)
-            return arr[0];
+    static int multiply(int n ){
+        if(n < 10)
+            return n;
+        return (n % 10) * multiply(n / 10);
 
-        return Math.min(arr[n - 1], min(arr, n - 1));
     }
     public static void main(String args[]){
-        int arr[] = {5, 2, 8, 1, 9};
-        System.out.println(min(arr, arr.length));
+        System.out.println(multiply(123));
     }
 }
