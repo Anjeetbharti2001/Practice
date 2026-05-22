738694d1f948caf8ba4d8216bba03d218d555f87
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-22 09:15:35 +0530

    fibonacci number

--- Today.java
+++ Today.java
@@ -1,17 +1,14 @@
-// climbing Stair Problem 
-
 public class Today{
     public static void main(String args[]){
-        int n = 5;
+        int n = 10;
         int dp[] = new int[n + 1];
 
-        dp[0] = 1;
+        dp[0] = 0;
         dp[1] = 1;
 
         for(int i = 2; i<= n; i++){
             dp[i] = dp[i - 1] + dp[i - 2];
-
         }
-        System.out.println("Total ways:-" + dp[n]);
+        System.out.println("Fibonacci number is : - " + dp[n]);
     }
 }
