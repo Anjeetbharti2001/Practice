53ffa1bbc818ba7bfcae843c46d432803170041c
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-22 09:06:24 +0530

    find minimum cost to reach

--- Today.java
+++ Today.java
@@ -1,25 +1,16 @@
 public class Today{
-    public static void main(String args[]){ 
-        int wt[] = {1,2,3};
-        int val[] =  {10, 15, 40};
-        int W = 6;
+    public static void main(String args[]){
+        int cost[] = {1,3,5,2};
+        int n = cost.length;
 
-        int n = wt.length;
-        int dp[][] = new int [n + 1][W + 1];
+        int dp[] = new int[n];
 
-        for(int i = 1; i<= n; i++){
-            for(int w = 1; w <= W; w++){
-                if(wt[i - 1] <= w){
-                    int include = val[i - 1] + dp[i - 1][w - wt[i - 1]];
+        dp[0] = cost[0];
 
-                    int exclude = dp[i - 1][w];
-                    dp[i][w] = Math.max(include, exclude);
-                }else{
-                    dp[i][w] = dp[i - 1][w];
-                }
-            }
+        for(int i = 1; i < n; i++){
+            dp[i] = cost[i] + dp[i - 1];
         }
-        System.out.println("Maximum profit :- " + dp[n][W]);
+        System.out.println("Minimum cost :- " + dp[n -1]);
 
     }
 }
