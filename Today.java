643d9810c88a0bedc09586b4094b92e52d1793b7
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-22 09:00:38 +0530

    0/1 Knapsack problem , Maximum value

--- Today.java
+++ Today.java
@@ -1,25 +1,25 @@
 public class Today{
-    static int matrixChain(int arr[]){
-        int n = arr.length;
-        int dp[][] = new int[n][n];
+    public static void main(String args[]){ 
+        int wt[] = {1,2,3};
+        int val[] =  {10, 15, 40};
+        int W = 6;
 
-        for(int len = 2; len < n ; len++){
-            for(int i = 1; i < n - len + 1; i++){
-                int j = i + len - 1;
+        int n = wt.length;
+        int dp[][] = new int [n + 1][W + 1];
 
-                dp[i][j] = Integer.MAX_VALUE;
+        for(int i = 1; i<= n; i++){
+            for(int w = 1; w <= W; w++){
+                if(wt[i - 1] <= w){
+                    int include = val[i - 1] + dp[i - 1][w - wt[i - 1]];
 
-                for(int k = i ; k < j ; k++){
-                    int cost = dp[i][k] + dp[k + 1][j] + arr[i - 1] * arr[k] *arr[j];
-                    dp[i][j] = Math.min(dp[i][j], cost);
+                    int exclude = dp[i - 1][w];
+                    dp[i][w] = Math.max(include, exclude);
+                }else{
+                    dp[i][w] = dp[i - 1][w];
                 }
             }
         }
-        return dp[1][n - 1];
-    }
-    public static void main(String args[]){
-         int arr[] =  {1,2,3,4};
+        System.out.println("Maximum profit :- " + dp[n][W]);
 
-         System.out.println("Minimum cost : " + matrixChain(arr));
-    } 
+    }
 }
