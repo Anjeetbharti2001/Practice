ffee6ebe3afab0a05ed1775560be0bd807091b79
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-22 09:20:49 +0530

    factorial number

--- Today.java
+++ Today.java
@@ -1,14 +1,13 @@
 public class Today{
     public static void main(String args[]){
-        int n = 10;
+        int n =  5;
         int dp[] = new int[n + 1];
 
-        dp[0] = 0;
-        dp[1] = 1;
+        dp[0] = 1;
 
-        for(int i = 2; i<= n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+        for(int i = 1; i<= n; i++){
+            dp[i] = i * dp[i - 1];
         }
-        System.out.println("Fibonacci number is : - " + dp[n]);
+        System.out.println("Factorial = " + dp[n]);
     }
 }
