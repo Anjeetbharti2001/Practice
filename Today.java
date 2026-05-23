ea907a36cc3cbb1c492adcf55fac3f14467b77e7
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-23 16:27:31 +0530

    simple function throught

--- Today.java
+++ Today.java
@@ -1,16 +1,11 @@
 public class Today{
+    // Function
+    static void greet(){
+        System.out.println("Hello anjeet ");
+    }
     public static void main(String args[]){
-        int coins[] = {1, 2, 3};
-        int amount = 4;
+        // function
+        greet();
 
-        int dp[] = new int[amount + 1];
-        dp[0] = 1;
-
-        for(int i = 0; i<coins.length; i++){
-            for(int j = coins[i]; j <= amount; j++){
-                dp[j] += dp[j - coins[i]];
-            }
-        }
-        System.out.println("Total ways = " + dp[amount]);
     }
 }
