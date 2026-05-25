c25a9108f8bc8d06d2bc948c5d2dcc9cfc1e61db
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-25 17:01:17 +0530

    Fast exponential bit

--- Today.java
+++ Today.java
@@ -1,20 +1,16 @@
-class Father{
-    void house(){
-        System.out.println("Fther has a house");
-    }
-}
-
-class Son extends Father{
-    void bike(){
-        System.out.println("Son has a bike ");
-    }
-}
-
 public class Today{
+    public static int fastExpo(int a, int n ){
+        int ans = 1;
+        while(n > 0){
+            if((n & 1) != 0){// check LSB
+               ans = ans * a;
+            }
+            a = a*a;
+            n = n>>1;
+        }
+        return ans;
+    }
     public static void main(String args[]){
-        Son s = new Son();
-
-        s.house();
-        s.bike();
+        System.out.println(fastExpo(3,5));
     }
 }
