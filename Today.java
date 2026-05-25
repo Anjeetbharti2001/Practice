cf51b91f9559ccd53155154ca28ae53563563d43
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-25 17:20:21 +0530

    clear ith bit

--- Today.java
+++ Today.java
@@ -1,9 +1,8 @@
 public class Today{
     public static void main(String args[]){
-        int n = 10; // 1010
-        int i = 0;
-
-        int result = n | (1 << i);
+        int n = 10;// 1010
+        int i = 1;
+        int result = n & ~(1 << i);
 
         System.out.println(result);
     }
