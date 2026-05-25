893e3fad4bb6bcbe89103e03870f9f356702b699
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-25 17:06:38 +0530

    Check whether a number is even or odd using bit manipulation

--- Today.java
+++ Today.java
@@ -1,16 +1,10 @@
 public class Today{
-    public static int fastExpo(int a, int n ){
-        int ans = 1;
-        while(n > 0){
-            if((n & 1) != 0){// check LSB
-               ans = ans * a;
-            }
-            a = a*a;
-            n = n>>1;
-        }
-        return ans;
-    }
     public static void main(String args[]){
-        System.out.println(fastExpo(3,5));
+        int n = 7;
+        if((n & 1) == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("odd");
+        }
     }
 }
