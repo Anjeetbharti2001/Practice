72743d54c819d8079482455a74433636ca062362
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-26 16:08:43 +0530

    with return types and Without Parameter

--- Today.java
+++ Today.java
@@ -1,7 +1,10 @@
 public class Today{
+    static int number(){
+        return 100;
+    }
     public static void main(String args[]){
-        int n = 8;
+        int result = number();
 
-        System.out.println(n >> 1);
+        System.out.println(result);
     }
 }
