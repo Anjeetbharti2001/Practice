5e02448480ba028412e5c5a9dbacdd202de6e884
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-27 20:17:32 +0530

    java Statement

--- Today.java
+++ Today.java
@@ -1,15 +1,8 @@
 public class Today{
     public static void main(String args[]){
-        String str = "education";
-        int count = 0;
-
-        for(int i = 0; i < str.length(); i++){
-            char ch = str.charAt(i);
-
-            if(ch== 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ){
-                count++;
-            }
-            System.out.println("Total vowels :-" + count);
+        int i = 10;
+        if(i < 15){
+            System.out.println("condition is True");
         }
     }
 }
