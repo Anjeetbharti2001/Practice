8df503b86f292d7e403c0f3b90e97aa9dd7068fd
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-27 19:26:28 +0530

    Reverse a string

--- Today.java
+++ Today.java
@@ -1,8 +1,12 @@
 public class Today{
-    static int square(int n){
-        return n * n ;
-    }
     public static void main(String args[]){
-        System.out.println(square(5));
+        String str = " hello ";
+        String rev = "";
+
+        for(int i = str.length() - 1; i >= 0; i--){
+            rev = rev + str.charAt(i);
+        }
+
+        System.out.println("Reversed String :-" + rev);
     }
 }
