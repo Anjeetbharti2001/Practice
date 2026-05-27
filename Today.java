25137115f16c1a9dbe2d873eb07479e37c1ba414
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-27 20:09:00 +0530

    Total vowels

--- Today.java
+++ Today.java
@@ -1,7 +1,15 @@
 public class Today{
     public static void main(String args[]){
-        String str = "Programming ";
+        String str = "education";
+        int count = 0;
 
-        System.out.println("Length = " + str.length());
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+
+            if(ch== 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ){
+                count++;
+            }
+            System.out.println("Total vowels :-" + count);
+        }
     }
 }
