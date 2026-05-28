84b809382e16c6d85e62225c0c51ff21ed87e985
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-28 12:35:24 +0530

    Add string

--- Today.java
+++ Today.java
@@ -1,11 +1,6 @@
 public class Today{
     public static void main(String args[]){
-        int age  = 20;
-        
-        if(age >= 28){
-            System.out.println("Adult");
-        }else{
-            System.out.println("minor");
-        }
+        String name = "Anjeet";
+        System.out.println("Bharti" + name);
     }
 }
