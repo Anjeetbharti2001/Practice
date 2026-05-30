1e58c1e5c03c3540667c01e7a5f842118b71a37b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-30 06:35:51 +0530

    Demonstrate Single Inheritance

--- Today.java
+++ Today.java
@@ -1,14 +1,18 @@
+// Write a program to demonstrate Single Inheritance.
 class Animal{
     void eat(){
-        System.out.println("Eating");
+        System.out.println("Animals is eating ");
     }
 }
 class Dog extends Animal{
+    void bark(){
+        System.out.println("Dog is Barking ");
+    }
 }
-
 public class Today{
     public static void main(String args[]){
         Dog d = new Dog();
         d.eat();
+        d.bark();
     }
 }
