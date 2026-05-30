2c1f769c8ddacef241a3fd5e8144640b41be63dd
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-05-30 06:46:48 +0530

    multilevel Inheritance

--- Today.java
+++ Today.java
@@ -1,18 +1,26 @@
-// Write a program to demonstrate Single Inheritance.
+// Write a program for multilevel Inheritance 
 class Animal{
     void eat(){
-        System.out.println("Animals is eating ");
+        System.out.println("Animal eat food");
     }
 }
 class Dog extends Animal{
     void bark(){
-        System.out.println("Dog is Barking ");
+        System.out.println("Dog barks");
     }
 }
+class Puppy extends Dog{
+    void weep(){
+        System.out.println("Puppy weeps");
+    }
+}
+
 public class Today{
-    public static void main(String args[]){
-        Dog d = new Dog();
-        d.eat();
-        d.bark();
+    public static void main(String [] args){
+        Puppy p = new Puppy();
+
+        p.eat();
+        p.bark();
+        p.weep();
     }
 }
