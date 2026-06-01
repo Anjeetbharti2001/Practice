39673d60c0d326ecd7222705fbe887aa4517ddcb
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-01 08:32:08 +0530

    PramidPattern2

--- Today.java
+++ Today.java
@@ -1,11 +1,15 @@
 public class Today{
     public static void main(String args[]){
-        int n = 10;
-        for(int i = 0; i<n; i++){
-            System.out.println("\n");
-            for(int j = 0; j<= i; j++){
+        int i, j, k = 8;
+        for(i = 0; i < 5; i++){
+            for(j = 0; j< k; j++){
+                System.out.print(" ");
+            }
+            k = k - 2;
+            for(j = 0; j<= i; j++){
                 System.out.print("*");
             }
+            System.out.println();
         }
     }
 }
