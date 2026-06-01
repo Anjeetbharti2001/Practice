35ad32e514373a6b7d155358438d4b0bebd76514
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-01 09:04:55 +0530

    Print Floyds triangle

--- Today.java
+++ Today.java
@@ -1,42 +1,19 @@
 import java.util.*;
 public class Today{
     public static void main(String args[]){
-        int n,i,j,k,l,m,p,q,r,s;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the n values");
-        n = sc.nextInt();
-        p = n;
-        q = n;
-        for(i = n; i>= 1; i++){
-            for(j = 1; j<= i; j++){
-                System.out.println("*");
+        System.out.println("Enter the number of rows\n");
+
+        int rows  = sc.nextInt();
+        System.out.println("Floyd's Triangle Generated\n");
+        int count = 1;
+        for(int i = 1; i<= rows; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(count + " ");
+
+                count++;
             }
-            for(k = p*2; k<n*2 -1; k++){
-                System.out.print(" ");
-            }
-            for( l=i; l!= 0; l--){
-                if(l==n){
-                    continue;
-                }
-                p--;
-                System.out.println();
-            }
-            for(i = 1; i<=n; i++){
-                for(j = 1; j<=i; j++){
-                    System.out.print("*");
-                }
-                for(k = q*2-2; k > 1; k--){
-                    System.out.print(" ");
-                }
-                for(m = i; m!= 0; m--){
-                    if(m == n){
-                        continue;
-                    }
-                    System.out.print("*");
-                }
-                System.out.println();
-                q--;
-            }
+            System.out.println();
         }
     }
 }
