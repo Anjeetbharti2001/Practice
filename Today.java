e3193140ac16ca9da023cdd2058f5d021bc6b205
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-01 08:08:02 +0530

    Program to print N numbers and find sum and average

--- Today.java
+++ Today.java
@@ -1,20 +1,17 @@
 public class Today{
     public static void main(String args[]){
-        int units = 123;
-        int bill = 0;
-
-        if(units > 100){
-            if(units >= 200){
-                if(units > 300){
-                    bill = units*8;
-                }else
-                    bill = units* 6;
+        int num = 0, i = 0;
+        System.out.println("Prime numbers from 1 to 100 are :");
+        for(i = 1; i<= 100; i++){
+            int counter = 0;
+            for(num = i; num>=1; num--){
+                if(i % num == 0){
+                    counter = counter + 1;
+                }
             }
-            else
-                bill = units *5;
+            if(counter == 2){
+                System.out.print(i + " ");
+            }
         }
-                System.out.println("CHENNAI ELECTRICITY LTD,CHENNAI");
-                System.out.println("Unit consumed :" + units);
-                System.out.println("Total bill :" + bill);
     }
 }
