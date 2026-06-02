3cf45f3565d16279b8d9c25a2b1e37e2bd22fbc8
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-02 10:55:42 +0530

    Simple java run

--- Today.java
+++ Today.java
@@ -1,16 +1,5 @@
 public class Today{
     public static void main(String args[]){
-        int [] a = new int[] {30, 30, 50, 4, 71, 100};
-        int max = a[0];
-        for(int i = 1; i < a.length; i++){
-            if(a[i] > max){
-                max = a[i];
-            }
-        }
-        System.out.println("The Given Arrays Element is:-");
-        for(int i = 0; i<a.length; i++){
-            System.out.println(a[i]);
-        }
-        System.out.println("From the arrays Element Largest Number is : " + max);
+        System.out.println("Anjeet in java for bignner");
     }
 }
