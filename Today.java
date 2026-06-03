0907e25382a3442b9392e3d0b57514af1772ccdd
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-03 08:31:47 +0530

    find largest and Smallest number in arrays

--- Today.java
+++ Today.java
@@ -1,22 +1,19 @@
-// Program to find second largest number in an arrays
+// Find largest and Smallest number in an arrays in java
 public class Today{
     public static void main(String args[]){
-        int arr[] = {14,46,47,86,92,52,48,36,66,85};
-        int largest = arr[0];
-        int secondLargest = arr[0];
+        int a[] = new int[]{23,34,13,64,72,90,10,15,9,27};
+        int min = a[0]; // assume first elements as smallest number
+        int max = a[0]; // assume first element as largest number
 
-        System.out.println("The given arrays is:");
-        for(int i = 0; i<arr.length; i++){
-            System.out.println(arr[i] + "\t");
-        }
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] > largest){
-                secondLargest = largest;
-                largest = arr[i];
-            }else if(arr[i] > secondLargest){
-                secondLargest = arr[i];
+        for(int i = 1; i<a.length; i++){
+            if(a[i]> max){
+                max = a[i];
+            }
+            if(a[i] < min){
+                min = a[i];
             }
         }
-        System.out.println("\nSecond largest Number is : " + secondLargest);
+        System.out.println("Largest Number in a given arrays is :" + max);
+        System.out.println("Smallest number in a given array is : " + min);
     }
 }
