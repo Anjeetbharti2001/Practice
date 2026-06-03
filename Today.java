f136bed5f44e2d9e05e4dd7310733d3930b6c9c8
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-03 08:38:47 +0530

    Find the index of The smallest number in an arrays

--- Today.java
+++ Today.java
@@ -1,19 +1,15 @@
-// Find largest and Smallest number in an arrays in java
+// find the index of the largest number in an arrays
 public class Today{
     public static void main(String args[]){
-        int a[] = new int[]{23,34,13,64,72,90,10,15,9,27};
-        int min = a[0]; // assume first elements as smallest number
-        int max = a[0]; // assume first element as largest number
-
-        for(int i = 1; i<a.length; i++){
-            if(a[i]> max){
+        int a[] = new int[]{12,44,23,56,23,78,13};
+        int max = a[0];
+        int index = 0;
+        for(int i = 0; i<a.length; i++){
+            if(max < a[i]){
                 max = a[i];
-            }
-            if(a[i] < min){
-                min = a[i];
+                index = i;
             }
         }
-        System.out.println("Largest Number in a given arrays is :" + max);
-        System.out.println("Smallest number in a given array is : " + min);
+        System.out.println("Indext Position of Maximum value in an is : " + index);
     }
 }
