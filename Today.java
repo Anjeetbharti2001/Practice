ef6fe83f52472bfbcdabf59d5d178d4151e65ffe
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-04 13:03:04 +0530

    Inorder Traversal of a tree

--- Today.java
+++ Today.java
@@ -7,17 +7,18 @@ class Node{
     }
 }
 public class Today{
-    static int count(Node root){
-        if(root == null)
-            return 0;
-
-        return 1 + count(root.left) + count(root.right);
+    static void inorder(Node root){
+        if(root != null){
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
     }
     public static void main(String args[]){
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
 
-        System.out.println("Total Nodes: " + count(root));
+        inorder(root);
     }
 }
