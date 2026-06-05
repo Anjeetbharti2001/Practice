38a13ce2a69a84a225472723a3570279806f79bb
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-05 10:19:19 +0530

    Math max

--- Today.java
+++ Today.java
@@ -1,9 +1,15 @@
+// public class Today{
+//     public static void main(String args[]){
+//         System.out.println("\nEnhanced for loop:");
+//         int[] numbers = {10,20,30,40,50};
+//         for(int num : numbers){
+//             System.out.println("num = " + num);
+//         }
+//     }
+// }
+
 public class Today{
     public static void main(String args[]){
-        System.out.println("\nEnhanced for loop:");
-        int[] numbers = {10,20,30,40,50};
-        for(int num : numbers){
-            System.out.println("num = " + num);
-        }
+        System.out.println(Math.max(5,10));
     }
 }
