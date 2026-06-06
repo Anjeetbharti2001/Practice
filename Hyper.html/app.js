0023dde53748d33d9c19eb857067e11c1fc9e631
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-06 12:01:19 +0530

    arrays print element

--- Hyper.html/app.js
+++ Hyper.html/app.js
@@ -1,11 +1,13 @@
-let url = "http://universities.hipolabs.com/search?name=";
-let country = "nepal";
+// let url = "http://universities.hipolabs.com/search?name=";
+// let country = "nepal";
 
-async function getColleges(){
-    try {
-       let res = await axios.get(url + country);
-       console.log(res);
-    } catch (e) {
-        console.log("error : " , e);
-    }
-}
+// async function getColleges(){
+//     try {
+//        let res = await axios.get(url + country);
+//        console.log(res);
+//     } catch (e) {
+//         console.log("error : " , e);
+//     }
+// }
+
+console.log("hello Anjeet, Welcome to Node.js");
--- Today.java
+++ Today.java
@@ -1,15 +1,9 @@
-// public class Today{
-//     public static void main(String args[]){
-//         System.out.println("\nEnhanced for loop:");
-//         int[] numbers = {10,20,30,40,50};
-//         for(int num : numbers){
-//             System.out.println("num = " + num);
-//         }
-//     }
-// }
-
 public class Today{
     public static void main(String args[]){
-        System.out.println(Math.max(5,10));
+        int [] arr = {10,20,30,40,50};
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
     }
 }
